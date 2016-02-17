fa4c266cb7ef5860999a8b5352b5de0c2c49651d
Ibrahim Hadeed <ibby93@gmail.com>
2016-02-17 04:49:26 -0500

    feat(): add clipboard plugin

---
+++ src/plugins/clipboard.ts
@@ -0,0 +1,53 @@
+import {Plugin, Cordova} from './plugin';
+
+/**
+ * Clipboard management plugin for Cordova that supports iOS, Android, and Windows Phone 8.
+ *
+ * Requires Cordova plugin: https://github.com/VersoSolutions/CordovaClipboard
+ *
+ * ```
+ * ionic plugin add https://github.com/VersoSolutions/CordovaClipboard.git
+ * ````
+ *
+ * @usage
+ * ```js
+ * Clipboard.copy("Hello world");
+ *
+ * Clipboard.paste().then(
+ *    (resolve : string) => {
+  *     alert(resolve);
+*     },
+ *     (reject : string) => {
+ *     alert("Error: " + reject);
+ *     }
+*     );
+ * );
+ * ```
+ */
+@Plugin({
+  name: 'Clipboard',
+  plugin: 'com.verso.cordova.clipboard',
+  pluginRef: 'cordova.plugins.clipboard'
+})
+export class Clipboard {
+
+  /**
+   * Copies the given text
+   * @param text
+   * @returns {Promise<T>}
+   */
+  @Cordova
+  static copy(text : string) : Promise<any> {
+    return new Promise((res, resj) => {});
+  }
+
+  /**
+   * Pastes the text stored in clipboard
+   * @returns {Promise<T>}
+   */
+  @Cordova
+  static paste() : Promise<any> {
+    return new Promise((res, rej) => {});
+  }
+
+}
