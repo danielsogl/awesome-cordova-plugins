3ce701cce7346b99ddffc12e505bc715f50ce9a6
Tim Lancina <tim@ionic.io>
2016-02-17 15:33:17 -0600

    chore(file): remove file for now

--- src/plugins/file.ts
+++ src/plugins/file.ts
@@ -1,11 +1,11 @@
-import {Plugin, Cordova} from './plugin';
-
-declare var window;
-
-@Plugin({
-  name: 'File',
-  plugin: 'cordova-plugin-file',
-  pluginRef: 'cordova.file'
-})
-export class File {
-}
+// import {Plugin, Cordova} from './plugin';
+//
+// declare var window;
+//
+// @Plugin({
+//   name: 'File',
+//   plugin: 'cordova-plugin-file',
+//   pluginRef: 'cordova.file'
+// })
+// export class File {
+// }
