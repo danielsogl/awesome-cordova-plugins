c7499185e02c953296cbde994b94965cb64f923f
Tim Lancina <tim@ionic.io>
2016-02-17 15:32:34 -0600

    chore(touchid): update TouchID

--- dist/plugins/touchid.js
+++ dist/plugins/touchid.js
@@ -5,14 +5,85 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 var plugin_1 = require('./plugin');
+/**
+ * Scan the fingerprint of a user with the TouchID sensor.
+ *
+ * Requires Cordova plugin: `cordova-plugin-touch-id`. For more info, please see the [TouchID plugin docs](https://github.com/EddyVerbruggen/cordova-plugin-touch-id).
+ *
+ * ### Error Codes
+ *
+ * The plugin will reject for various reasons. Your app will most likely need to respond to the cases differently.
+ *
+ * Here is a list of some of the error codes:
+ *
+ *    `-1` - Fingerprint scan failed more than 3 times
+ *    `-2` or `-128` - User tapped the 'Cancel' button
+ *    `-3` - User tapped the 'Enter Passcode' or 'Enter Password' button
+ *    `-4` - The scan was cancelled by the system (Home button for example)
+ *    `-6` - TouchID is not Available
+ *    `-8` - TouchID is locked out from too many tries
+ *
+ */
 var TouchID = (function () {
     function TouchID() {
     }
-    TouchID.prototype.isAvailable = function () { };
+    /**
+     * Whether TouchID is available or not.
+     *
+     * @return {Promise} Returns a Promise that resolves if yes, rejects if no.
+     */
+    TouchID.prototype.isAvailable = function () {
+        // This Promise is replaced by one from the @Cordova decorator that wraps
+        // the plugin's callbacks. We provide a dummy one here so TypeScript
+        // knows that the correct return type is Promise, because there's no way
+        // for it to know the return type from a decorator.
+        // See https://github.com/Microsoft/TypeScript/issues/4881
+        return new Promise(function (res, rej) { });
+    };
     ;
-    TouchID.verifyFingerprint = function (message) { };
-    TouchID.verifyFingerprintWithCustomPasswordFallback = function (message) { };
-    TouchID.verifyFingerprintWithCustomPasswordFallbackAndEnterPasswordLabel = function (message, enterPasswordLabel) { };
+    /**
+     * Show TouchID dialog and wait for a fingerprint scan. If user taps 'Enter Password' button, brings up standard system passcode screen.
+     *
+     * @param {string} message  The message to display
+     * @return {Promise} Returns a Promise the resolves if the fingerprint scan was successful, rejects with an error code (see above).
+     */
+    TouchID.verifyFingerprint = function (message) {
+        // This Promise is replaced by one from the @Cordova decorator that wraps
+        // the plugin's callbacks. We provide a dummy one here so TypeScript
+        // knows that the correct return type is Promise, because there's no way
+        // for it to know the return type from a decorator.
+        // See https://github.com/Microsoft/TypeScript/issues/4881
+        return new Promise(function (res, rej) { });
+    };
+    /**
+     * Show TouchID dialog and wait for a fingerprint scan. If user taps 'Enter Password' button, rejects with code '-3' (see above).
+     *
+     * @param {string} message  The message to display
+     * @return {Promise} Returns a Promise the resolves if the fingerprint scan was successful, rejects with an error code (see above).
+     */
+    TouchID.verifyFingerprintWithCustomPasswordFallback = function (message) {
+        // This Promise is replaced by one from the @Cordova decorator that wraps
+        // the plugin's callbacks. We provide a dummy one here so TypeScript
+        // knows that the correct return type is Promise, because there's no way
+        // for it to know the return type from a decorator.
+        // See https://github.com/Microsoft/TypeScript/issues/4881
+        return new Promise(function (res, rej) { });
+    };
+    /**
+     * Show TouchID dialog with custom 'Enter Password' message and wait for a fingerprint scan. If user taps 'Enter Password' button, rejects with code '-3' (see above).
+     *
+     * @param {string} message  The message to display
+     * @param {string} enterPasswordLabel  Custom text for the 'Enter Password' button
+     * @return {Promise} Returns a Promise the resolves if the fingerprint scan was successful, rejects with an error code (see above).
+     */
+    TouchID.verifyFingerprintWithCustomPasswordFallbackAndEnterPasswordLabel = function (message, enterPasswordLabel) {
+        // This Promise is replaced by one from the @Cordova decorator that wraps
+        // the plugin's callbacks. We provide a dummy one here so TypeScript
+        // knows that the correct return type is Promise, because there's no way
+        // for it to know the return type from a decorator.
+        // See https://github.com/Microsoft/TypeScript/issues/4881
+        return new Promise(function (res, rej) { });
+    };
     __decorate([
         plugin_1.Cordova()
     ], TouchID.prototype, "isAvailable", null);
