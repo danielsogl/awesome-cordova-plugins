8562195533b20cb950d1ffcd6816a57178848819
Tim Lancina <tim@ionic.io>
2016-02-18 12:01:27 -0600

    docs(actionsheet): update

--- dist/plugins/actionsheet.js
+++ dist/plugins/actionsheet.js
@@ -7,23 +7,41 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var plugin_1 = require('./plugin');
 /**
  * The ActionSheet plugin shows a native list of options the user can choose from.
+ *
+ * Requires Cordova plugin: `cordova-plugin-actionsheet`. For more info, please see the [ActionSheet plugin docs](https://github.com/phonegap/phonegap-plugin-barcodescanner).
+ *
+ * @usage
+ *
+ * ```
+ * import {ActionSheet} from 'ionic-native';
+ *
+ * let buttonLabels = ['Share via Facebook', 'Share via Twitter'];
+ * ActionSheet.show({
+ *   'title': 'What do you want with this image?',
+ *   'buttonLabels': buttonLabels,
+ *   'addCancelButtonWithLabel': 'Cancel',
+ *   'addDestructiveButtonWithLabel' : 'Delete'
+ * }).then(buttonIndex => {
+ *   console.log('Button pressed: ' + buttonLabels[buttonIndex - 1]);
+ * });
+ * ```
+ *
  */
 var ActionSheet = (function () {
     function ActionSheet() {
     }
     /**
      * Show the ActionSheet.
-     * @param {options}
-     * Available options:
-     *   buttonLabels: string[]
-     *   title: string
-     *   androidTheme (Android only): number 1-5
-     *   androidEnableCancelButton (Android only): boolean, default false
-     *   winphoneEnableCancelButton (WP only): boolean, default false
-     *   addCancelButtonWithLabel: string
-     *   addDestructiveButtonWithLabel: string
-     *   position: [x, y] (iPad pass in [x, y] coords of popover)
-     * @returns {Promise} returns a promise that resolves with the index of the
+     * @param {options} options
+     *   `buttonLabels`: string[]
+     *   `title`: string
+     *   `androidTheme` (Android only): number 1-5
+     *   `androidEnableCancelButton` (Android only): boolean, default false
+     *   `winphoneEnableCancelButton` (WP only): boolean, default false
+     *   `addCancelButtonWithLabel`: string
+     *   `addDestructiveButtonWithLabel`: string
+     *   `position`: [x, y] (iPad pass in [x, y] coords of popover)
+     * @returns {Promise} Returns a Promise that resolves with the index of the
      *   button pressed (1 based, so 1, 2, 3, etc.)
      */
     ActionSheet.show = function (options) {
@@ -34,7 +52,6 @@ var ActionSheet = (function () {
         // See https://github.com/Microsoft/TypeScript/issues/4881
         return new Promise(function (res, rej) { });
     };
-    ;
     /**
      * Hide the ActionSheet.
      */
@@ -46,7 +63,6 @@ var ActionSheet = (function () {
         // See https://github.com/Microsoft/TypeScript/issues/4881
         return new Promise(function (res, rej) { });
     };
-    ;
     __decorate([
         plugin_1.Cordova()
     ], ActionSheet, "show", null);
--- src/plugins/actionsheet.ts
+++ src/plugins/actionsheet.ts
@@ -2,6 +2,25 @@ import {Plugin, Cordova} from './plugin';
 
 /**
  * The ActionSheet plugin shows a native list of options the user can choose from.
+ *
+ * Requires Cordova plugin: `cordova-plugin-actionsheet`. For more info, please see the [ActionSheet plugin docs](https://github.com/phonegap/phonegap-plugin-barcodescanner).
+ *
+ * @usage
+ *
+ * ```
+ * import {ActionSheet} from 'ionic-native';
+ *
+ * let buttonLabels = ['Share via Facebook', 'Share via Twitter'];
+ * ActionSheet.show({
+ *   'title': 'What do you want with this image?',
+ *   'buttonLabels': buttonLabels,
+ *   'addCancelButtonWithLabel': 'Cancel',
+ *   'addDestructiveButtonWithLabel' : 'Delete'
+ * }).then(buttonIndex => {
+ *   console.log('Button pressed: ' + buttonLabels[buttonIndex - 1]);
+ * });
+ * ```
+ *
  */
 @Plugin({
   name: 'ActionSheet',
@@ -13,17 +32,16 @@ export class ActionSheet {
 
   /**
    * Show the ActionSheet.
-   * @param {options}
-   * Available options:
-   *   buttonLabels: string[]
-   *   title: string
-   *   androidTheme (Android only): number 1-5
-   *   androidEnableCancelButton (Android only): boolean, default false
-   *   winphoneEnableCancelButton (WP only): boolean, default false
-   *   addCancelButtonWithLabel: string
-   *   addDestructiveButtonWithLabel: string
-   *   position: [x, y] (iPad pass in [x, y] coords of popover)
-   * @returns {Promise} returns a promise that resolves with the index of the
+   * @param {options} options
+   *   `buttonLabels`: string[]  
+   *   `title`: string  
+   *   `androidTheme` (Android only): number 1-5  
+   *   `androidEnableCancelButton` (Android only): boolean, default false  
+   *   `winphoneEnableCancelButton` (WP only): boolean, default false  
+   *   `addCancelButtonWithLabel`: string  
+   *   `addDestructiveButtonWithLabel`: string  
+   *   `position`: [x, y] (iPad pass in [x, y] coords of popover)  
+   * @returns {Promise} Returns a Promise that resolves with the index of the
    *   button pressed (1 based, so 1, 2, 3, etc.)
    */
   @Cordova()
@@ -43,7 +61,7 @@ export class ActionSheet {
     // for it to know the return type from a decorator.
     // See https://github.com/Microsoft/TypeScript/issues/4881
     return new Promise<any>((res, rej) => {});
-  };
+  }
 
 
   /**
@@ -57,5 +75,5 @@ export class ActionSheet {
     // for it to know the return type from a decorator.
     // See https://github.com/Microsoft/TypeScript/issues/4881
     return new Promise<any>((res, rej) => {});
-  };
+  }
 }
