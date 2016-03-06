bc4dcaae535b12a2c2a62f44d7d894c2d99bb598
Ibrahim Hadeed <ibby93@gmail.com>
2016-03-06 15:35:59 -0500

    feat(plugin): add functionality to app-availablity plugin

--- src/plugins/appavailability.ts
+++ src/plugins/appavailability.ts
@@ -17,4 +17,19 @@ import {Plugin, Cordova} from './plugin';
 })
 export class AppAvailability {
 
+  /**
+   * Checks if an app is available on device
+   * @param app Package name on android, or URI scheme on iOS
+   * @returns {Promise<boolean>}
+   */
+  @Cordova()
+  static check(app : string) : Promise<any> {
+    // This Promise is replaced by one from the @Cordova decorator that wraps
+    // the plugin's callbacks. We provide a dummy one here so TypeScript
+    // knows that the correct return type is Promise, because there's no way
+    // for it to know the return type from a decorator.
+    // See https://github.com/Microsoft/TypeScript/issues/4881
+    return new Promise<boolean>((res, rej) => {});
+  }
+
 }
