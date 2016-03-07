37aaa3d09cda6a885b35943554f7febd3c8403fd
Tim Behety <tim.behety@gmail.com>
2016-03-07 09:04:33 +0100

    Fix link of ActionSheet documentation

--- src/plugins/actionsheet.ts
+++ src/plugins/actionsheet.ts
@@ -5,7 +5,7 @@ import {Plugin, Cordova} from './plugin';
  * @description
  * The ActionSheet plugin shows a native list of options the user can choose from.
  *
- * Requires Cordova plugin: `cordova-plugin-actionsheet`. For more info, please see the [ActionSheet plugin docs](https://github.com/phonegap/phonegap-plugin-barcodescanner).
+ * Requires Cordova plugin: `cordova-plugin-actionsheet`. For more info, please see the [ActionSheet plugin docs](https://github.com/EddyVerbruggen/cordova-plugin-actionsheet).
  *
  * @usage
  *
