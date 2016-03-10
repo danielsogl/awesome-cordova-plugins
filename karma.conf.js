e5eea1618f1324da2fb505dbdf30fb8592e78c0f
Ibrahim Hadeed <ibby93@gmail.com>
2016-03-10 16:31:35 -0500

    chore(test): set up tests

--- karma.conf.js
+++ karma.conf.js
@@ -58,14 +58,14 @@ module.exports = function(config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: [
-      'Chrome',
-      //'PhantomJS'
+      //'Chrome',
+      'PhantomJS'
     ],
 
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
