99f86547e2080741a2cf7c065a995bdc298f594f
Ibrahim Hadeed <ibby93@gmail.com>
2016-03-10 16:36:05 -0500

    test(mocks): add mock cordova plugins object

---
+++ test/mocks/plugins.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Ibrahim on 3/10/2016.
+ */
+
+
+window['cordova'] = {
+
+        plugins : {
+
+            actionsheet : {
+                show: function() {},
+                hide: function() {}
+            },
+
+            appAvailability : {
+                check: function() {}
+            }
+
+        }
+
+}
