4d8dc1b9dc8e9191b32db17471dfc27932123aca
Ibrahim Hadeed <ibby93@gmail.com>
2016-03-12 18:04:30 -0500

    feat(plugins): add new plugins to index

--- src/index.ts
+++ src/index.ts
@@ -3,44 +3,65 @@ initAngular1();
 
 const DEVICE_READY_TIMEOUT = 2000;
 
-
 declare var window;
 
 import {ActionSheet} from './plugins/actionsheet';
+import {AppAvailability} from './plugins/appavailability';
 import {AppRate} from './plugins/apprate';
 import {AppVersion} from './plugins/appversion';
-import {BarcodeScanner} from './plugins/barcodescanner';
 import {Badge} from './plugins/badge';
+import {BarcodeScanner} from './plugins/barcodescanner';
+import {BatteryStatus} from './plugins/batterystatus';
 import {BLE} from './plugins/ble';
-import {Camera} from './plugins/camera';
 import {Calendar} from './plugins/calendar';
+import {Camera} from './plugins/camera';
 import {Clipboard} from './plugins/clipboard';
 import {Contacts} from './plugins/contacts';
 import {DatePicker} from './plugins/datepicker';
 import {Device} from './plugins/device';
+import {DeviceMotion} from './plugins/devicemotion';
+import {DeviceOrientation} from './plugins/deviceorientation';
+import {Dialogs} from './plugins/dialogs';
 import {Facebook} from './plugins/facebook';
+//import {File} from './plugins/file';
+import {Flashlight} from './plugins/flashlight';
 import {Geolocation} from './plugins/geolocation';
+import {ImagePicker} from './plugins/imagepicker';
+import {LaunchNavigator} from './plugins/launchnavigator';
+import {LocalNotifications} from './plugins/localnotifications';
 import {Push} from './plugins/push';
+import {SMS} from './plugins/sms';
 import {StatusBar} from './plugins/statusbar';
 import {Toast} from './plugins/toast';
 import {TouchID} from './plugins/touchid';
 
 export {
   ActionSheet,
+  AppAvailability,
   AppRate,
   AppVersion,
-  BarcodeScanner,
   Badge,
+  BarcodeScanner,
+  BatteryStatus,
   BLE,
-  Camera,
   Calendar,
+  Camera,
   Clipboard,
   Contacts,
   DatePicker,
   Device,
+  DeviceMotion,
+  DeviceOrientation,
+  Dialogs,
   Facebook,
+  //File,
+  Flashlight,
   Geolocation,
+  ImagePicker,
+  LaunchNavigator,
+  LocalNotifications,
   Push,
+  SMS,
   StatusBar,
   Toast,
   TouchID
@@ -51,20 +72,31 @@ export * from './plugins/plugin';
 // Window export to use outside of a module loading system
 window['IonicNative'] = {
   ActionSheet: ActionSheet,
+  AppAvailability: AppAvailability,
   AppRate: AppRate,
   AppVersion: AppVersion,
-  BarcodeScanner: BarcodeScanner,
   Badge: Badge,
+  BarcodeScanner: BarcodeScanner,
+  BatteryStatus: BatteryStatus,
   BLE: BLE,
-  Camera: Camera,
   Calendar: Calendar,
+  Camera: Camera,
   Clipboard: Clipboard,
   Contacts: Contacts,
   DatePicker: DatePicker,
   Device: Device,
+  DeviceMotion: DeviceMotion,
+  DeviceOrientation: DeviceOrientation,
+  Dialogs: Dialogs,
   Facebook: Facebook,
+  //File: File,
+  Flashlight: Flashlight,
   Geolocation: Geolocation,
+  ImagePicker: ImagePicker,
+  LaunchNavigator: LaunchNavigator,
+  LocalNotifications: LocalNotifications,
   Push: Push,
+  SMS: SMS,
   StatusBar: StatusBar,
   Toast: Toast,
   TouchID: TouchID
