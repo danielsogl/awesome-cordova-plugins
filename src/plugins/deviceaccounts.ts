0a7577cec0b4e3d5b2c717ebb721ea3031809575
Ibrahim Hadeed <ibby93@gmail.com>
2016-04-29 17:28:02 -0400

    refactor(DeviceAccounts): formatting

--- src/plugins/deviceaccounts.ts
+++ src/plugins/deviceaccounts.ts
@@ -1,33 +1,32 @@
 import {Cordova, Plugin} from './plugin';
-declare var window;
 @Plugin({
   plugin: 'https://github.com/loicknuchel/cordova-device-accounts.git',
   pluginRef: 'plugins.DeviceAccounts',
-  repo: 'https://github.com/loicknuchel/cordova-device-accounts.git'
+  repo: 'https://github.com/loicknuchel/cordova-device-accounts'
 })
 export class DeviceAccounts {
 
-/**
- *  Gets all accounts registered on the Android Device
- */
+  /**
+   *  Gets all accounts registered on the Android Device
+   */
   @Cordova()
   static get() : Promise<any> {return}
 
-/**
- *  Get all accounts registred on Android device for requested type
- */
+  /**
+   *  Get all accounts registered on Android device for requested type
+   */
   @Cordova()
   static getByType(type: string) : Promise<any> {return}
 
-/**
- *  Get all emails registred on Android device (accounts with 'com.google' type)
- */
+  /**
+   *  Get all emails registered on Android device (accounts with 'com.google' type)
+   */
   @Cordova()
   static getEmails() : Promise<any> {return}
 
-/**
- *  Get the first email registred on Android device
- */
+  /**
+   *  Get the first email registered on Android device
+   */
   @Cordova()
   static getEmail() : Promise<any> {return}
 }
