4b4334fef1ae101ffa320ed7ee2d1511e1009a83
Ibby Hadeed <ibby93@gmail.com>
2016-06-26 11:19:29 -0400

    docs(): cleanup docs

--- src/plugins/diagnostic.ts
+++ src/plugins/diagnostic.ts
@@ -19,21 +19,21 @@ export class Diagnostic {
   @Cordova()
   static isWifiEnabled(): Promise<any> {return; }
 
-  /*
+  /**
    * Checks if the device has a camera. On Android this returns true if the device has a camera. On iOS this returns true if both the device has a camera AND the application is authorized to use it. On Windows 10 Mobile this returns true if both the device has a rear-facing camera AND the
    * application is authorized to use it.
    */
   @Cordova()
   static isCameraEnabled(): Promise<any> {return; }
 
-  /*
+  /**
    * Checks if the device has Bluetooth capabilities and if so that Bluetooth is switched on (same on Android, iOS and Windows 10 Mobile)
    * On Android this requires permission <uses-permission android:name="android.permission.BLUETOOTH" />
    */
   @Cordova()
   static isBluetoothEnabled(): Promise<any> {return; }
 
-  /*
+  /**
    * Returns the location authorization status for the application.
    * Note for Android: this is intended for Android 6 / API 23 and above. Calling on Android 5 / API 22 and below will always return GRANTED status as permissions are already granted at installation time.
    *
@@ -42,28 +42,26 @@ export class Diagnostic {
   @Cordova()
   static requestLocationAuthorization(mode?: string): Promise<any> {return; }
 
-  /*
+  /**
    * Checks if the application is authorized to use location.
    * Note for Android: this is intended for Android 6 / API 23 and above. Calling on Android 5 / API 22 and below will always return GRANTED status as permissions are already granted at installation time.
    */
   @Cordova()
   static isLocationAuthorized(): Promise<any> {return; }
 
-
-
-  /*
+  /**
    * Checks if camera hardware is present on device.
    */
   @Cordova()
   static isCameraPresent(): Promise<any> {return; }
 
-  /*
+  /**
    * Checks if the application is authorized to use the camera.
    * Note for Android: this is intended for Android 6 / API 23 and above. Calling on Android 5 / API 22 and below will always return TRUE as permissions are already granted at installation time.
    */
   @Cordova()
   static isCameraAuthorized(): Promise<any> {return; }
-  /*
+  /**
    * Checks if location mode is set to return high-accuracy locations from GPS hardware.
    *   Returns true if Location mode is enabled and is set to either:
    *   - Device only = GPS hardware only (high accuracy)
@@ -72,7 +70,7 @@ export class Diagnostic {
   @Cordova()
   static isGpsLocationEnabled(): Promise<any> {return; }
 
-  /*
+  /**
    * Checks if location mode is set to return low-accuracy locations from network triangulation/WiFi access points.
    * Returns true if Location mode is enabled and is set to either:
    *   - Battery saving = network triangulation and Wifi network IDs (low accuracy)
@@ -82,7 +80,6 @@ export class Diagnostic {
   static isNetworkLocationEnabled(): Promise<any> {return; }
 
   /**
-   *
    * Checks if remote (push) notifications are enabled.
    * On iOS 8+, returns true if app is registered for remote notifications AND "Allow Notifications" switch is ON AND alert style is not set to "None" (i.e. "Banners" or "Alerts").
    * On iOS <=7, returns true if app is registered for remote notifications AND alert style is not set to "None" (i.e. "Banners" or "Alerts") - same as isRegisteredForRemoteNotifications().
@@ -90,16 +87,11 @@ export class Diagnostic {
   @Cordova()
   static isRemoteNotificationsEnabled(): Promise<any> {return; }
 
-
   /**
-   *
    * Indicates if the app is registered for remote (push) notifications on the device.
    * On iOS 8+, returns true if the app is registered for remote notifications and received its device token, or false if registration has not occurred, has failed, or has been denied by the user. Note that user preferences for notifications in the Settings app will not affect this.
    * On iOS <=7, returns true if app is registered for remote notifications AND alert style is not set to "None" (i.e. "Banners" or "Alerts") - same as isRemoteNotificationsEnabled().
    */
   @Cordova()
   static isRegisteredForRemoteNotifications(): Promise<any> {return; }
-
-
-
 }
