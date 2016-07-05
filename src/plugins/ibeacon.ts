302dceef86c34306f489cc8265ba0c2a41ffc08e
Justin Schuldt <justinschuldt@gmail.com>
2016-07-05 14:18:09 -0500

    moar methods

--- src/plugins/ibeacon.ts
+++ src/plugins/ibeacon.ts
@@ -34,15 +34,15 @@ export interface PluginResult {
   state: string;
 }
 export interface Delegate {
-  didDetermineStateForRegion(pluginResult?: PluginResult): Observable<PluginResult>;
-  didStartMonitoringForRegion(pluginResult?: PluginResult): Observable<PluginResult>;
-  didExitRegion(pluginResult?: PluginResult): Observable<PluginResult>;
-  didEnterRegion(pluginResult?: PluginResult): Observable<PluginResult>;
-  didRangeBeaconsInRegion(pluginResult?: PluginResult): Observable<PluginResult>;
-  peripheralManagerDidStartAdvertising(pluginResult?: PluginResult): Observable<PluginResult>;
-  peripheralManagerDidUpdateState(pluginResult?: PluginResult): Observable<PluginResult>;
-  didChangeAuthorizationStatus(authorizationStatus?: string): Observable<string>;
-  monitoringDidFailForRegionWithError(pluginResult?: PluginResult): Observable<PluginResult>;
+  didChangeAuthorizationStatus(): Observable<string>;
+  didDetermineStateForRegion(): Observable<PluginResult>;
+  didEnterRegion(): Observable<PluginResult>;
+  didExitRegion(): Observable<PluginResult>;
+  didRangeBeaconsInRegion(): Observable<PluginResult>;
+  didStartMonitoringForRegion(): Observable<PluginResult>;
+  monitoringDidFailForRegionWithError(): Observable<PluginResult>;
+  peripheralManagerDidStartAdvertising(): Observable<PluginResult>;
+  peripheralManagerDidUpdateState(): Observable<PluginResult>;
 }
 
 @Plugin({
@@ -57,137 +57,126 @@ export class IBeacon {
     console.log('IBeacon constructor ran');
   }
 
-  static init(): Delegate {
+  static Delegate(): Delegate {
     let delegate = new cordova.plugins.locationManager.Delegate();
 
-    // let didEnterRegionSubject = new Subject<PluginResult>();
-    // delegate.didEnterRegion$ = didEnterRegionSubject.asObservable();
-    // delegate.didEnterRegion = (pluginResult: PluginResult) => didEnterRegionSubject.next(pluginResult);
-    delegate.didEnterRegion = (pluginResult?: PluginResult) => {
-      console.log('inside didStartMonitoringForRegion function');
+    delegate.didChangeAuthorizationStatus = (pluginResult?: PluginResult) => {
       return new Observable<PluginResult>(
         (observer: any) => {
-          console.log('inside didEnterRegion observer function');
+          let cb = (data: PluginResult) => observer.next(data);
+          return delegate.didChangeAuthorizationStatus = cb;
+        }
+      );
+    };
+
+    delegate.didDetermineStateForRegion = (pluginResult?: PluginResult) => {
+      return new Observable<PluginResult>(
+        (observer: any) => {
+          let cb = (data: PluginResult) => observer.next(data);
+          return delegate.didDetermineStateForRegion = cb;
+        }
+      );
+    };
+
+    delegate.didEnterRegion = (pluginResult?: PluginResult) => {
+      return new Observable<PluginResult>(
+        (observer: any) => {
           let cb = (data: PluginResult) => observer.next(data);
           return delegate.didEnterRegion = cb;
         }
       );
     };
 
-    delegate.didStartMonitoringForRegion = (pluginResult?: PluginResult) => {
-      console.log('inside didStartMonitoringForRegion function');
-
+    delegate.didExitRegion = (pluginResult?: PluginResult) => {
       return new Observable<PluginResult>(
         (observer: any) => {
-          console.log('inside didStartMonitoringForRegion observer function');
+          let cb = (data: PluginResult) => observer.next(data);
+          return delegate.didExitRegion = cb;
+        }
+      );
+    };
+
+    delegate.didRangeBeaconsInRegion = (pluginResult?: PluginResult) => {
+      return new Observable<PluginResult>(
+        (observer: any) => {
+          let cb = (data: PluginResult) => observer.next(data);
+          return delegate.didRangeBeaconsInRegion = cb;
+        }
+      );
+    };
+
+    delegate.didStartMonitoringForRegion = (pluginResult?: PluginResult) => {
+      return new Observable<PluginResult>(
+        (observer: any) => {
           let cb = (data: PluginResult) => observer.next(data);
           return delegate.didStartMonitoringForRegion = cb;
         }
       );
     };
 
-    // delegate.didRangeBeaconsInRegion = (pluginResult: PluginResult) => { return () => pluginResult; };
-    let funSubject = new Subject<PluginResult>();
-
-    delegate.didRangeBeaconsInRegion = function(pluginResult){
-      funSubject.next(pluginResult);
-      return funSubject.asObservable();
+    delegate.monitoringDidFailForRegionWithError = (pluginResult?: PluginResult) => {
+      return new Observable<PluginResult>(
+        (observer: any) => {
+          let cb = (data: PluginResult) => observer.next(data);
+          return delegate.monitoringDidFailForRegionWithError = cb;
+        }
+      );
     };
 
-    delegate.didDetermineStateForRegion = function(pluginResult): void {
-      console.debug('didDetermineStateForRegion pluginResult: ', pluginResult);
+    delegate.peripheralManagerDidStartAdvertising = (pluginResult?: PluginResult) => {
+      return new Observable<PluginResult>(
+        (observer: any) => {
+          let cb = (data: PluginResult) => observer.next(data);
+          return delegate.peripheralManagerDidStartAdvertising = cb;
+        }
+      );
+    };
+
+    delegate.peripheralManagerDidUpdateState = (pluginResult?: PluginResult) => {
+      return new Observable<PluginResult>(
+        (observer: any) => {
+          let cb = (data: PluginResult) => observer.next(data);
+          return delegate.peripheralManagerDidUpdateState = cb;
+        }
+      );
     };
 
     cordova.plugins.locationManager.setDelegate(delegate);
     return delegate;
   }
 
-  static init2(): Delegate {
-    let delegate = new cordova.plugins.locationManager.Delegate();
-
-    let newDel: Delegate;
-    newDel.didEnterRegion = (): Observable<PluginResult> => {
-      return new Observable<PluginResult>(
-        (observer: any) => {
-          console.log('init()2 inside didEnterRegion observer function');
-          let cb = (data: PluginResult) => observer.next(data);
-          return delegate.didEnterRegion = cb;
-        }
-      );
-    };
-    newDel.didStartMonitoringForRegion = () => {
-      return new Observable<PluginResult>(
-        (observer: any) => {
-          console.log('init()2 inside didStartMonitoringForRegion observer function');
-          let cb = (data: PluginResult) => observer.next(data);
-          return delegate.didStartMonitoringForRegion = cb;
-        }
-      );
-    };
-    newDel.didRangeBeaconsInRegion = () => {
-      return new Observable<PluginResult>(
-        (observer: any) => {
-          console.log('init()2 inside didStartMonitoringForRegion observer function');
-          let cb = (data: PluginResult) => observer.next(data);
-          return delegate.didRangeBeaconsInRegion = cb;
-        }
-      );
-    };
-    delegate.didDetermineStateForRegion = function(pluginResult): void {
-      console.debug('init()2 didDetermineStateForRegion pluginResult: ', pluginResult);
-    };
-
-    cordova.plugins.locationManager.setDelegate(delegate);
-    return newDel;
-   }
-
-   static BeaconRegion(beaconRegion: BeaconRegion): BeaconRegion {
-     return new cordova.plugins.locationManager.BeaconRegion(beaconRegion.identifer, beaconRegion.uuid);
-   }
-
-  static startMonitoringForRegion(region: BeaconRegion): void {
-    cordova.plugins.locationManager.startMonitoringForRegion(region)
-      .fail(function(e) { console.error(e); })
-      .done(() => console.log('started monitoring'));
-    return;
+  static BeaconRegion(identifer: string, uuid: string, minor?: number, major?: number, notifyEntryStateOnDisplay?: boolean): BeaconRegion {
+    return new cordova.plugins.locationManager.BeaconRegion(identifer, uuid, minor, major, notifyEntryStateOnDisplay);
   }
 
-  @Cordova()
+  @Cordova({sync: true})
+  static startMonitoringForRegion(region: BeaconRegion): Promise<string> { return; }
+
+  @Cordova({sync: true})
   static stopMonitoringForRegion(region: BeaconRegion): Promise<void> { return; }
 
-  @Cordova()
-  static startRangingBeaconsInRegion(region: BeaconRegion): void {
-    cordova.plugins.locationManager.startRangingBeaconsInRegion(region)
-      .fail(function(e) { console.error(e); })
-      .done(() => console.log('started ranging'));
-    return;
-  }
+  @Cordova({sync: true})
+  static startRangingBeaconsInRegion(region: BeaconRegion): void { return; }
 
-  // @Cordova()
-  // Q promises dont play nicely
-  static enableDebugLogs(): Promise<void> {
-    cordova.plugins.locatiionManager.enableDebugLogs()
-      .fail(function(e) { console.error(e); })
-      .done(() => console.log('enabled debug logs'));
-    return;
-  }
+  @Cordova({sync: true})
+  static stopRangingBeaconsInRegion(region: BeaconRegion): void { return; }
 
-  @Cordova()
+  @Cordova({sync: true})
+  static enableDebugLogs(): Promise<void> { return; }
+
+  @Cordova({sync: true})
   static disableDebugLogs(): Promise<void> { return; }
 
-  // @Cordova()
-  // Q promises dont play nicely
-  static enableDebugNotifications(): Promise<void> {
-    cordova.plugins.locatiionManager.enableDebugNotifications()
-      .fail(function(e) { console.error(e); })
-      .done(() => console.log('enabled debug notifications'));
-    return;
-  }
+  @Cordova({sync: true})
+  static enableDebugNotifications(): Promise<void> { return; }
 
-  @Cordova()
+  @Cordova({sync: true})
   static disableDebugNotifications(): Promise<void> { return; }
 
-  @Cordova()
+  @Cordova({sync: true})
   static appendToDeviceLog(message: string): Promise<void> { return; }
+
+  @Cordova()
+  static requestAlwaysAuthorization(): Promise<void> { return; }
 
 }
