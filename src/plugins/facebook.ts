63a1a34f9777f1c3e4ee44ad9457b282efcb5c2c
Guille <darkwilly92@gmail.com>
2016-07-08 00:59:05 +0200

    refactor(facebook4):

--- src/plugins/facebook.ts
+++ src/plugins/facebook.ts
@@ -1,4 +1,4 @@
-import {Plugin, Cordova} from './plugin';
+import { Cordova, Plugin } from './plugin';
 
 /**
  * @name Facebook
@@ -85,15 +85,15 @@ import {Plugin, Cordova} from './plugin';
 })
 export class Facebook {
 
-    /**
-     * Browser wrapper
-     * @param appId
-     * @param version
-     */
-   @Cordova()
-   static browserInit(appId: number, version?: string): Promise<any> {
-     return;
-   }
+  /**
+   * Browser wrapper
+   * @param appId
+   * @param version
+   */
+  @Cordova()
+  static browserInit(appId: number, version?: string): Promise<any> {
+    return;
+  }
 
   /**
    * Login to Facebook to authenticate this app.
@@ -213,7 +213,7 @@ export class Facebook {
     name: string,
     params?: Object,
     valueToSum?: number
-  ): Promise<any> { return; }
+    ): Promise<any> { return; }
 
   /**
    * Log a purchase. For more information see the Events section above.
