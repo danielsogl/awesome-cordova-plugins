93e79cd052a5fbb8f1cca9962981a49d39e87a06
DGoms <dgomesh@gmail.com>
2016-07-11 18:46:26 +0200

    Update index.ts
    Unable to import interface like FileUploadOptions

--- src/index.ts
+++ src/index.ts
@@ -80,6 +80,7 @@ export * from './plugins/datepicker';
 export * from './plugins/device';
 export * from './plugins/devicemotion';
 export * from './plugins/deviceorientation';
+export * from './plugins/filetransfer';
 export * from './plugins/googlemaps';
 export * from './plugins/httpd';
 export * from './plugins/imagepicker';
