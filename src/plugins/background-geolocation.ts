4f0cd244765518b88c472fe22a366411fa8a4217
Ibby Hadeed <ibby93@gmail.com>
2016-07-11 17:40:51 -0400

    fix(geolocation): fix plugin reference
    closes #258

--- src/plugins/background-geolocation.ts
+++ src/plugins/background-geolocation.ts
@@ -177,7 +177,7 @@ export interface Config {
  *             debug: true, //  enable this hear sounds for background-geolocation life-cycle.
  *             stopOnTerminate: false, // enable this to clear background location settings when the app terminates
  *     };
- * 
+ *
  *     BackgroundGeolocation.configure(config)
  *        .then((location) => {
  *             console.log('[js] BackgroundGeolocation callback:  ' + location.latitude + ',' + location.longitude);
@@ -202,7 +202,7 @@ export interface Config {
  */
 @Plugin({
   plugin: 'cordova-plugin-mauron85-background-geolocation',
-  pluginRef: 'plugins.backgroundGeoLocation',
+  pluginRef: 'plugins.backgroundGeolocation',
   repo: 'https://github.com/mauron85/cordova-plugin-background-geolocation',
   platforms: ['iOS', 'Android', 'Windows Phone 8']
 })
