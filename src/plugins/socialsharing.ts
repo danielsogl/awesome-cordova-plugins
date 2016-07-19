c13abab8e4c3f08361fdc424440f0e06901adf4a
Kim Biesbjerg <kb@mindly.dk>
2016-07-19 17:11:58 +0200

    - Add support for canShareViaEmail - Make the following parameters in shareViaEmail method optional: cc, bcc, files - Add usage for canShareViaEmail and shareViaEmail - Fixed some formatting to be more consistent

--- src/plugins/socialsharing.ts
+++ src/plugins/socialsharing.ts
@@ -5,11 +5,22 @@ import {Plugin, Cordova} from './plugin';
  * @description
  * Share text, files, images, and links via social networks, sms, and email.
  * @usage
- * ```js
+ * ```ts
  * import {SocialSharing} from 'ionic-native';
  *
- * ...
- * // TODO add usage info
+ * // Check if sharing via email is supported
+ * SocialSharing.canShareViaEmail().then(() => {
+ *   // Sharing via email is possible
+ * }).catch(() => {
+ *   // Sharing via email is not possible
+ * });
+ *
+ * // Share via email
+ * SocialSharing.shareViaEmail('Body', 'Subject', 'recipient@example.org').then(() => {
+ *   // Success!
+ * }).catch(() => {
+ *   // Error!
+ * });
  * ```
  */
 @Plugin({
@@ -19,17 +30,15 @@ import {Plugin, Cordova} from './plugin';
   platforms: ['iOS', 'Android', 'Windows Phone']
 })
 export class SocialSharing {
-
-
   /**
    * Shares using the share sheet
    * @param message {string} The message you would like to share.
    * @param subject {string} The subject
-   * @param file {string|Array<string>} URL(s) to file(s) or image(s), local path(s) to file(s) or image(s), or base64 data of an image. Only the first file/image will be used on Windows Phone.
+   * @param file {string|string[]} URL(s) to file(s) or image(s), local path(s) to file(s) or image(s), or base64 data of an image. Only the first file/image will be used on Windows Phone.
    * @param url {string} A URL to share
    */
   @Cordova()
-  static share (message?: string, subject?: string, file?: string|Array<string>, url?: string): Promise<any> {return; }
+  static share(message?: string, subject?: string, file?: string|string[], url?: string): Promise<any> { return; }
 
   /**
    * Shares using the share sheet with additional options and returns a result object or an error message (requires plugin version 5.1.0+)
@@ -38,7 +47,7 @@ export class SocialSharing {
   @Cordova({
     platforms: ['iOS', 'Android']
   })
-  static shareWithOptions (options: { message?: string, subject?: string, file?: string|Array<string>, url?: string, chooserTitle?: string }): Promise<any> {return; }
+  static shareWithOptions(options: { message?: string, subject?: string, file?: string|string[], url?: string, chooserTitle?: string }): Promise<any> { return; }
 
   /**
    * Checks if you can share via a specific app.
@@ -47,7 +56,7 @@ export class SocialSharing {
   @Cordova({
     platforms: ['iOS', 'Android']
   })
-  static canShareVia (appName: string): Promise<any> {return; }
+  static canShareVia(appName: string): Promise<any> { return; }
 
   /**
    * Shares directly to Twitter
@@ -58,7 +67,7 @@ export class SocialSharing {
   @Cordova({
     platforms: ['iOS', 'Android']
   })
-  static shareViaTwitter (message: string, image?: string, url?: string): Promise<any> {return; }
+  static shareViaTwitter(message: string, image?: string, url?: string): Promise<any> { return; }
 
   /**
    * Shares directly to Facebook
@@ -69,7 +78,7 @@ export class SocialSharing {
   @Cordova({
     platforms: ['iOS', 'Android']
   })
-  static shareViaFacebook (message: string, image?: string, url?: string): Promise<any> {return; }
+  static shareViaFacebook(message: string, image?: string, url?: string): Promise<any> { return; }
 
 
   /**
@@ -82,7 +91,7 @@ export class SocialSharing {
   @Cordova({
     platforms: ['iOS', 'Android']
   })
-  static shareViaFacebookWithPasteMessageHint (message: string, image?: string, url?: string, pasteMessageHint?: string): Promise<any> {return; }
+  static shareViaFacebookWithPasteMessageHint(message: string, image?: string, url?: string, pasteMessageHint?: string): Promise<any> { return; }
 
   /**
    * Shares directly to Instagram
@@ -92,7 +101,7 @@ export class SocialSharing {
   @Cordova({
     platforms: ['iOS', 'Android']
   })
-  static shareViaInstagram (message: string, image: string): Promise<any> {return; }
+  static shareViaInstagram(message: string, image: string): Promise<any> { return; }
 
   /**
    * Shares directly to WhatsApp
@@ -103,7 +112,7 @@ export class SocialSharing {
   @Cordova({
     platforms: ['iOS', 'Android']
   })
-  static shareViaWhatsApp (message: string, image?: string, url?: string): Promise<any> {return; }
+  static shareViaWhatsApp(message: string, image?: string, url?: string): Promise<any> { return; }
 
   /**
    * Shares directly to a WhatsApp Contact
@@ -115,7 +124,7 @@ export class SocialSharing {
   @Cordova({
     platforms: ['iOS', 'Android']
   })
-  static shareViaWhatsAppToReceiver (receiver: string, message: string, image?: string, url?: string): Promise<any> {return; }
+  static shareViaWhatsAppToReceiver(receiver: string, message: string, image?: string, url?: string): Promise<any> { return; }
 
   /**
    * Share via SMS
@@ -125,21 +134,28 @@ export class SocialSharing {
   @Cordova({
     platforms: ['iOS', 'Android']
   })
-  static shareViaSMS(messge: string, phoneNumber: string): Promise<any> {return; }
+  static shareViaSMS(messge: string, phoneNumber: string): Promise<any> { return; }
+
+  /**
+   * Checks if you can share via email
+   */
+  @Cordova({
+    platforms: ['iOS', 'Android']
+  })
+  static canShareViaEmail(): Promise<any> { return; }
 
   /**
    * Share via Email
    * @param message {string}
    * @param subject {string}
-   * @param to {Array<string>}
-   * @param cc {Array<string>}
-   * @param bcc {Array<string>}
-   * @param files {string|Array<string>} URL or local path to file(s) to attach
+   * @param to {string[]}
+   * @param cc {string[]}
+   * @param bcc {string[]}
+   * @param files {string|string[]} URL or local path to file(s) to attach
    */
   @Cordova({
     platforms: ['iOS', 'Android']
   })
-  static shareViaEmail(message: string, subject: string, to: Array<string>, cc: Array<string>, bcc: Array<string>, files: string|Array<string>): Promise<any> {return; }
-
+  static shareViaEmail(message: string, subject: string, to: string[], cc: string[] = [], bcc: string[] = [], files: string|string[] = []): Promise<any> { return; }
 
 }
