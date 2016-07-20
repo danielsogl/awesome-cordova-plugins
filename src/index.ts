fda77a28b97da34c8086c4c7c7b7d755ab2fa4fc
Ibby Hadeed <ibby93@gmail.com>
2016-07-20 05:45:26 -0400

    feat(full-screen-image): add FullScreenImage plugin

--- src/index.ts
+++ src/index.ts
@@ -38,6 +38,7 @@ import {Facebook} from './plugins/facebook';
 import {File} from './plugins/file';
 import {Transfer} from './plugins/filetransfer';
 import {Flashlight} from './plugins/flashlight';
+import {FullScreenImage} from './plugins/full-screen-image';
 import {Geolocation} from './plugins/geolocation';
 import {Globalization} from './plugins/globalization';
 import {GooglePlus} from './plugins/google-plus';
@@ -126,6 +127,7 @@ export {
   Facebook,
   File,
   Flashlight,
+  FullScreenImage,
   Geolocation,
   Globalization,
   GooglePlus,
@@ -186,6 +188,7 @@ window['IonicNative'] = {
   Facebook: Facebook,
   File: File,
   Flashlight: Flashlight,
+  FullScreenImage: FullScreenImage,
   Geolocation: Geolocation,
   Globalization: Globalization,
   GooglePlus: GooglePlus,
