d4bee49678cc1bf2ea9771f8800555fd4442dca9
Ibby Hadeed <ibby93@gmail.com>
2016-07-20 07:12:44 -0400

    fix(base64togallery): update plugin wrapper to match latest version
    closes #335

--- src/plugins/base64togallery.ts
+++ src/plugins/base64togallery.ts
@@ -24,11 +24,11 @@ export class Base64ToGallery {
   /**
    * Converts a base64 string to an image file in the device gallery
    * @param {string} data The actual base64 string that you want to save
-   * @param {string} prefix Prefix the file with a string. Default is 'img_'. Optional.
+   * @param {any} options (optional) An object with properties: prefix, mediaScanner
    * @returns {Promise} returns a promise that resolves when the image is saved.
    */
   @Cordova()
-  static base64ToGallery(data: string, prefix?: string): Promise<any> {
+  static base64ToGallery(data: string, options?: {prefix?: string; mediaScanner?: boolean}): Promise<any> {
     return;
   }
 
