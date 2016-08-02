990c9c2a4915cf2825d95d4d038d9daafce3cd26
Max Lynch <max@drifty.com>
2016-08-02 11:23:39 -0500

    Bower script

--- scripts/bower.sh
+++ scripts/bower.sh
@@ -19,6 +19,7 @@ cp dist/ionic.native.js dist/ionic.native.min.js $DIR
 cd $DIR
 git init
 git remote add origin git@github.com:driftyco/ionic-native-bower.git
+cp ../bower.json .
 git add .
 git commit -m "Bower release"
 git tag -f -m v$VERSION v$VERSION
