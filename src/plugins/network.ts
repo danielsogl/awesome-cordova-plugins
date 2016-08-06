3748bccad2844283ae9ea1494f5a8e91405fb698
Keith D. Moore <keith@kdmooreconsulting.com>
2016-08-06 09:56:34 -0500

    Network: Changed connection to return a Typescript string

--- src/plugins/network.ts
+++ src/plugins/network.ts
@@ -54,7 +54,7 @@ export class Network {
    * Return the network connection type
    */
   @CordovaProperty
-  static get connection(): String { return navigator.connection.type; }
+  static get connection(): string { return navigator.connection.type; }
 
   /**
    * Get notified when the device goes offline
