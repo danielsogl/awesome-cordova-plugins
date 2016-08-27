bffa76ed76ed613711396fd6e07be2221d069250
Ibrahim Hadeed <ibby93@gmail.com>
2016-08-27 00:31:02 -0400

    feat(call-number): add support for CallNumber plugin

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,7 @@ import { Brightness } from './plugins/brightness';
 import { BLE } from './plugins/ble';
 import { BluetoothSerial } from './plugins/bluetoothserial';
 import { Calendar } from './plugins/calendar';
+import { CallNumber } from './plugins/call-number';
 import { Camera } from './plugins/camera';
 import { CameraPreview } from './plugins/camera-preview';
 import { CardIO } from './plugins/card-io';
@@ -143,6 +144,7 @@ BatteryStatus,
 Brightness,
 BLE,
 BluetoothSerial,
+CallNumber,
 CameraPreview,
 Clipboard,
 CodePush,
@@ -208,6 +210,7 @@ window['IonicNative'] = {
   BLE: BLE,
   BluetoothSerial: BluetoothSerial,
   Calendar: Calendar,
+  CallNumber: CallNumber,
   Camera: Camera,
   CameraPreview: CameraPreview,
   CardIO: CardIO,
