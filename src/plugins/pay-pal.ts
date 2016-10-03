7355fa24966ae336763111c78bee26bbe5b949c2
Ibby <ibby93@gmail.com>
2016-10-03 16:51:57 -0400

    more docs

--- src/plugins/pay-pal.ts
+++ src/plugins/pay-pal.ts
@@ -177,8 +177,17 @@ export declare var PayPalItem: {
 };
 
 export interface PayPalPaymentDetails {
+  /**
+   * Sub-total (amount) of items being paid for. 10 characters max with support for 2 decimal places.
+   */
   subtotal: string;
+  /**
+   * Amount charged for shipping. 10 characters max with support for 2 decimal places.
+   */
   shipping: string;
+  /**
+   * Amount charged for tax. 10 characters max with support for 2 decimal places.
+   */
   tax: string;
 }
 
@@ -192,21 +201,102 @@ export declare var PayPalPaymentDetails: {
   new(subtotal: string, shipping: string, tax: string): PayPalPaymentDetails;
 };
 
+/**
+ * @private
+ */
 export interface PayPalConfigurationOptions {
+  /**
+   * Will be overridden by email used in most recent PayPal login.
+   */
   defaultUserEmail?: string;
+  /**
+   * Will be overridden by phone country code used in most recent PayPal login
+   */
   defaultUserPhoneCountryCode?: string;
+  /**
+   * Will be overridden by phone number used in most recent PayPal login.
+   */
   defaultUserPhoneNumber?: string;
+  /**
+   * Your company name, as it should be displayed to the user when requesting consent via a PayPalFuturePaymentViewController.
+   */
   merchantName?: string;
+  /**
+   * URL of your company's privacy policy, which will be offered to the user when requesting consent via a PayPalFuturePaymentViewController.
+   */
   merchantPrivacyPolicyUrl?: string;
+  /**
+   * URL of your company's user agreement, which will be offered to the user when requesting consent via a PayPalFuturePaymentViewController.
+   */
   merchantUserAgreementUrl?: string;
+  /**
+   * If set to NO, the SDK will only support paying with PayPal, not with credit cards.
+   * This applies only to single payments (via PayPalPaymentViewController).
+   * Future payments (via PayPalFuturePaymentViewController) always use PayPal.
+   * Defaults to true
+   */
   acceptCreditCards?: boolean;
+  /**
+   * For single payments, options for the shipping address.
+   * - 0 - PayPalShippingAddressOptionNone: no shipping address applies.
+   * - 1 - PayPalShippingAddressOptionProvided: shipping address will be provided by your app,
+   *   in the shippingAddress property of PayPalPayment.
+   * - 2 - PayPalShippingAddressOptionPayPal: user will choose from shipping addresses on file
+   *   for their PayPal account.
+   * - 3 - PayPalShippingAddressOptionBoth: user will choose from the shipping address provided by your app,
+   *   in the shippingAddress property of PayPalPayment, plus the shipping addresses on file for the user's PayPal account.
+   * Defaults to 0 (PayPalShippingAddressOptionNone).
+   */
   payPalShippingAddressOption?: number;
+  /**
+   * If set to YES, then if the user pays via their PayPal account,
+   * the SDK will remember the user's PayPal username or phone number;
+   * if the user pays via their credit card, then the SDK will remember
+   * the PayPal Vault token representing the user's credit card.
+   *
+   * If set to NO, then any previously-remembered username, phone number, or
+   * credit card token will be erased, and subsequent payment information will
+   * not be remembered.
+   *
+   * Defaults to YES.
+   */
   rememberUser?: boolean;
+  /**
+   * If not set, or if set to nil, defaults to the device's current language setting.
+   *
+   * Can be specified as a language code ("en", "fr", "zh-Hans", etc.) or as a locale ("en_AU", "fr_FR", "zh-Hant_HK", etc.).
+   * If the library does not contain localized strings for a specified locale, then will fall back to the language. E.g., "es_CO" -> "es".
+   * If the library does not contain localized strings for a specified language, then will fall back to American English.
+   *
+   * If you specify only a language code, and that code matches the device's currently preferred language,
+   * then the library will attempt to use the device's current region as well.
+   * E.g., specifying "en" on a device set to "English" and "United Kingdom" will result in "en_GB".
+   */
   languageOrLocale?: string;
+  /**
+   * Normally, the SDK blurs the screen when the app is backgrounded,
+   * to obscure credit card or PayPal account details in the iOS-saved screenshot.
+   * If your app already does its own blurring upon backgrounding, you might choose to disable this.
+   * Defaults to NO.
+   */
   disableBlurWhenBackgrounding?: boolean;
+  /**
+   * If you will present the SDK's view controller within a popover, then set this property to YES.
+   * Defaults to NO. (iOS only)
+   */
   presentingInPopover?: boolean;
+  /**
+   * Sandbox credentials can be difficult to type on a mobile device. Setting this flag to YES will
+   * cause the sandboxUserPassword and sandboxUserPin to always be pre-populated into login fields.
+   */
   forceDefaultsInSandbox?: boolean;
-  sandboxUserPAssword?: string;
+  /**
+   * Password to use for sandbox if 'forceDefaultsInSandbox' is set.
+   */
+  sandboxUserPassword?: string;
+  /**
+   * PIN to use for sandbox if 'forceDefaultsInSandbox' is set.
+   */
   sandboxUserPin?: string;
 }
 /**
@@ -219,8 +309,25 @@ export declare var PayPalConfiguration: {
    */
   new(options: PayPalConfigurationOptions): PayPalConfiguration;
 };
+/**
+ * @private
+ */
 export interface PayPalConfiguration {
-
+  defaultUserEmail: string;
+  defaultUserPhoneCountryCode: string;
+  defaultUserPhoneNumber: string;
+  merchantName: string;
+  merchantPrivacyPolicyUrl: string;
+  merchantUserAgreementUrl: string;
+  acceptCreditCards: boolean;
+  payPalShippingAddressOption: number;
+  rememberUser: boolean;
+  languageOrLocale: string;
+  disableBlurWhenBackgrounding: boolean;
+  presentingInPopover: boolean;
+  forceDefaultsInSandbox: boolean;
+  sandboxUserPassword: string;
+  sandboxUserPin: string;
 }
 /**
  * @private
@@ -230,7 +337,7 @@ export declare var PayPalShippingAddress: {
    * See the documentation of the individual properties for more detail.
    * @param {String} recipientName: Name of the recipient at this address. 50 characters max.
    * @param {String} line1: Line 1 of the address (e.g., Number, street, etc). 100 characters max.
-   * @param {String} Line 2 of the address (e.g., Suite, apt #, etc). 100 characters max. Optional.
+   * @param {String} line2: Line 2 of the address (e.g., Suite, apt #, etc). 100 characters max. Optional.
    * @param {String} city: City name. 50 characters max.
    * @param {String} state: 2-letter code for US states, and the equivalent for other countries. 100 characters max. Required in certain countries.
    * @param {String} postalCode: ZIP code or equivalent is usually required for countries that have them. 20 characters max. Required in certain countries.
@@ -238,12 +345,36 @@ export declare var PayPalShippingAddress: {
    */
   new(recipientName: string, line1: string, line2: string, city: string, state: string, postalCode: string, countryCode: string): PayPalShippingAddress;
 };
+/**
+ * @private
+ */
 export interface PayPalShippingAddress {
+  /**
+   * Name of the recipient at this address. 50 characters max.
+   */
   recipientName: string;
+  /**
+   * Line 1 of the address (e.g., Number, street, etc). 100 characters max.
+   */
   line1: string;
+  /**
+   * Line 2 of the address (e.g., Suite, apt #, etc). 100 characters max. Optional.
+   */
   line2: string;
+  /**
+   * City name. 50 characters max.
+   */
   city: string;
+  /**
+   * 2-letter code for US states, and the equivalent for other countries. 100 characters max. Required in certain countries.
+   */
   state: string;
+  /**
+   * ZIP code or equivalent is usually required for countries that have them. 20 characters max. Required in certain countries.
+   */
   postalCode: string;
+  /**
+   * 2-letter country code. 2 characters max.
+   */
   countryCode: string;
 }
