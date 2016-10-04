5c1146bfde3871f4bf0c71d64b11117beedf07b8
Andreas Gassmann <andreas@andreasgassmann.ch>
2016-10-04 13:35:11 +0200

    fix(zBar): fix typings and improve docs

--- src/plugins/z-bar.ts
+++ src/plugins/z-bar.ts
@@ -16,11 +16,13 @@ import { Plugin, Cordova } from './plugin';
  *       drawSight: false
  *     };
  *
- * ZBar.scan(zBarOptions, result => {
+ * ZBar.scan(zBarOptions)
+ *    .then(result => {
  *       console.log(result); // Scanned code
- *     }, error => {
+ *    })
+ *    .catch(error => {
  *       console.log(error); // Error message
- *     });
+ *    });
  *
  * ```
  *
@@ -47,21 +49,43 @@ export class ZBar {
   /**
    * Open the scanner
    * @param options { ZBarOptions } Scan options
-   * @param success { ZBarSuccessCallback } Success callback
-   * @param failure { ZBarErrorCallback } Error callback
+   * @return Returns a Promise that resolves with the scanned string, or rejects with an error.
    */
   @Cordova()
-  static scan(options: ZBarOptions, success: ZBarSuccessCallback, failure: ZBarErrorCallback): void { return; }
+  static scan(options: ZBarOptions): Promise<any> { return; }
 
 }
 
-export interface ZBarSuccessCallback { (scanResult: string): void; }
-export interface ZBarErrorCallback { (error: string): void; }
-
 export interface ZBarOptions {
+  /**
+   * A string representing the title text (Android only).
+   * Default: "Scan QR Code"
+   */
   text_title?: string;
+
+  /**
+   * A string representing the instruction text (Android only).
+   * Default: "Please point your camera at the QR code."
+   */
   text_instructions?: string;
+
+  /**
+   * A string defining the active camera when opening the scanner.
+   * Possible values: "front", "back"
+   * Default: "back"
+   */
   camera?: string;
+
+  /**
+   * A string defining the state of the flash.
+   * Possible values: "on", "off", "auto"
+   * Default: "auto"
+   */
   flash?: string;
+
+  /**
+   * A boolean to show or hide a line in the center of the scanner.
+   * Default: true
+   */
   drawSight?: boolean;
 }
