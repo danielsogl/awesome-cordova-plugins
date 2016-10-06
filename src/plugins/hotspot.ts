d79d62bfa0bf9addde747278ebd1e2cca6a8f24e
Ibby <ibby93@gmail.com>
2016-10-05 22:46:22 -0400

    docs(hotspot): remove unecessary doc tags

--- src/plugins/hotspot.ts
+++ src/plugins/hotspot.ts
@@ -217,27 +217,27 @@ export class Hotspot {
 
 export interface ConnectionInfo {
   /**
-   * @property {string}   SSID
+   *   SSID
    *      The service set identifier (SSID) of the current 802.11 network.
    */
   SSID: string;
   /**
-   * @property {string}   BSSID
+   *   BSSID
    *      The basic service set identifier (BSSID) of the current access point.
    */
   BSSID: string;
   /**
-   * @property {string}   linkSpeed
+   *   linkSpeed
    *      The current link speed in Mbps
    */
   linkSpeed: string;
   /**
-   * @property {string}   IPAddress
+   *   IPAddress
    *      The IP Address
    */
   IPAddress: string;
   /**
-   * @property {string}   networkID
+   *   networkID
    *      Each configured network has a unique small integer ID, used to identify the network when performing operations on the supplicant.
    */
   networkID: string;
@@ -245,62 +245,62 @@ export interface ConnectionInfo {
 
 export interface HotspotNetwork {
   /**
-   * @property {string}       SSID
+   *       SSID
    *      Human readable network name
    */
   SSID: string;
   /**
-   * @property {string}       BSSID
+   *       BSSID
    *      MAC Address of the access point
    */
   BSSID: string;
   /**
-   * @property {number (int)} frequency
+   * frequency
    *      The primary 20 MHz frequency (in MHz) of the channel over which the client is communicating with the access point.
    */
   frequency: number;
   /**
-   * @property {number}       level
+   *       level
    *      The detected signal level in dBm, also known as the RSSI.
    */
   level: number;
   /**
-   * @property {number}       timestamp
+   *       timestamp
    *      Timestamp in microseconds (since boot) when this result was last seen.
    */
   timestamp: number;
   /**
-   * @property {string}       capabilities
+   *       capabilities
    *      Describes the authentication, key management, and encryption schemes supported by the access point.
    */
   capabilities: string;
 }
 export interface HotspotNetworkConfig {
   /**
-   * @property {string}   deviceIPAddress - Device IP Address
+   *   deviceIPAddress - Device IP Address
    */
   deviceIPAddress: string;
   /**
-   * @property {string}   deviceMacAddress - Device MAC Address
+   *   deviceMacAddress - Device MAC Address
    */
   deviceMacAddress: string;
   /**
-   * @property {string}   gatewayIPAddress - Gateway IP Address
+   *   gatewayIPAddress - Gateway IP Address
    */
   gatewayIPAddress: string;
   /**
-   * @property {string}   gatewayMacAddress - Gateway MAC Address
+   *   gatewayMacAddress - Gateway MAC Address
    */
   gatewayMacAddress: string;
 }
 export interface HotspotDevice {
   /**
-   * @property {string}   ip
+   *   ip
    *      Hotspot IP Address
    */
   ip: string;
   /**
-   * @property {string}   mac
+   *   mac
    *      Hotspot MAC Address
    */
   mac: string;
