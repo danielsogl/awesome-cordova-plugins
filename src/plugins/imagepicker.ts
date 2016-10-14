db3d5b63c6945980aa334ad14ef3ee2d95055c2a
Ibby <ibby93@gmail.com>
2016-10-14 06:44:57 -0400

    docs(image-picker): add interface docs

--- src/plugins/imagepicker.ts
+++ src/plugins/imagepicker.ts
@@ -2,19 +2,23 @@ import { Cordova, Plugin } from './plugin';
 
 
 export interface ImagePickerOptions {
-  // max images to be selected, defaults to 15. If this is set to 1, upon
-  // selection of a single image, the plugin will return it.
+  /**
+   * max images to be selected, defaults to 15. If this is set to 1, upon selection of a single image, the plugin will return it.
+   */
   maximumImagesCount?: number;
 
-  // max width and height to allow the images to be.  Will keep aspect
-  // ratio no matter what.  So if both are 800, the returned image
-  // will be at most 800 pixels wide and 800 pixels tall.  If the width is
-  // 800 and height 0 the image will be 800 pixels wide if the source
-  // is at least that wide.
+  /**
+   * Max width to allow images to be
+   */
   width?: number;
+  /**
+   * Max height to allow images to be
+   */
   height?: number;
 
-  // quality of resized image, defaults to 100
+  /**
+   * Quality of images, defaults to 100
+   */
   quality?: number;
 }
 
@@ -38,6 +42,8 @@ export interface ImagePickerOptions {
  *   }
  * }, (err) => { });
  * ```
+ * @interfaces
+ * ImagePickerOptions
  */
 @Plugin({
   plugin: 'cordova-plugin-image-picker',
