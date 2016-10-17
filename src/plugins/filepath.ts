d1f5d2adb9f52b006f398084c4106e23e024de63
fco <colin.frick@kyberna.com>
2016-10-17 20:36:16 +0200

    feat(filepath): add cordova-plugin-filepath

---
+++ src/plugins/filepath.ts
@@ -0,0 +1,34 @@
+import { Plugin, Cordova } from './plugin';
+
+declare var window: any;
+
+/**
+ * @name FilePath
+ * @description
+ *
+ * This plugin allows you to resolve the native filesystem path for Android content URIs and is based on code in the aFileChooser library.
+ *
+ * @usage
+ * ```
+ * import {FilePath} from 'ionic-native';
+ *
+ * FilePath.resolveNativePath(path)
+ *   .then(filePath => console.log(filePath);
+ *   .catch(err => console.log(err);
+ *
+ * ```
+ */
+@Plugin({
+  plugin: 'cordova-plugin-filepath',
+  pluginRef: 'window.FilePath',
+  repo: 'https://github.com/hiddentao/cordova-plugin-filepath',
+  platforms: ['Android']
+})
+export class FilePath {
+  /**
+   * Resolve native path for given content URL/path.
+   * @param {String} path  Content URL/path.
+   */
+  @Cordova()
+  static resolveNativePath(path: string): Promise<string> {return; }
+}
