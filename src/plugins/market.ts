273e9276c05f2ff4c5f9cc552a8e833d672f2980
Afranio Martins <afranioce@gmail.com>
2016-11-03 16:55:13 -0300

    feat(market) add static method search

--- src/plugins/market.ts
+++ src/plugins/market.ts
@@ -2,13 +2,17 @@ import { Plugin, Cordova } from './plugin';
 /**
  * @name Market
  * @description
- * Opens an app's page in the market place (Google Play, App Store)
+ * Opens or search an app's page in the market place (Google Play, App Store)
  *
  * @usage
  * ```
  * import {Market} from 'ionic-native';
  *
  * Market.open('your.package.name');
+ * 
+ * //or
+ * 
+ * Market.search('keyword');
  *
  * ```
  */
@@ -27,4 +31,11 @@ export class Market {
   @Cordova({sync: true})
   static open(appId: string, callbacks?: {success?: Function, failure?: Function}): void { }
 
+  /**
+   * Search apps in Google Play / App Store
+   * @param key {string} Keyword
+   * @param callbacks {Object} Optional callbacks
+   */
+  @Cordova({sync: true})
+  static search(key: string, callbacks?: {success?: Function, failure?: Function}): void { }
 }
