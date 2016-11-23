695099b2b097a2c5d25dc2eed20f29456d6e94dc
Yasin Simsek <yasin-simsek@gmx.de>
2016-11-23 10:48:08 +0100

    fix(3dTouch): fixes onHomeIconPressed (#813)

--- src/plugins/3dtouch.ts
+++ src/plugins/3dtouch.ts
@@ -108,7 +108,7 @@ export class ThreeDeeTouch {
    */
   static onHomeIconPressed(): Observable<any> {
     return new Observable(observer => {
-      if (window.ThreeDeeTouch && window.ThreeDeeTouch.onHomeIconPressed) {
+      if (window.ThreeDeeTouch) {
         window.ThreeDeeTouch.onHomeIconPressed = observer.next.bind(observer);
       } else {
         observer.error('3dTouch plugin is not available.');
