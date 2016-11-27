f7e307b19e043f0333c994cf6641d2f95e66fa21
Wyatt Arent <hello@wyatt.ninja>
2016-11-27 09:46:15 -0700

    Update camera-preview.ts
    Sending preview to back is initially confusing and causes it to appear to not work.

--- src/plugins/camera-preview.ts
+++ src/plugins/camera-preview.ts
@@ -40,7 +40,7 @@ export interface CameraPreviewSize {
  *   'front', // default camera
  *   true, // tap to take picture
  *   false, // disable drag
- *   true, // send the preview to the back of the screen so we can add overlaying elements
+ *   false, // Keep preview in front. Set to false (back of the screen) to apply overlaying elements
  *   1 // set the preview alpha
  * );
  *
