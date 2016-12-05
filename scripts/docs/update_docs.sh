c29aaca857595059e3491f44acd0340cbdccae66
Perry Govier <perrygovier@users.noreply.github.com>
2016-12-05 16:43:40 -0600

    chore(ci): test

--- scripts/docs/update_docs.sh
+++ scripts/docs/update_docs.sh
@@ -24,7 +24,8 @@ function run {
   cd $SITE_DIR
 
   # if no changes, don't commit
-  if [ -z "$(git status --porcelain)" ]; then
+  if git diff-index --quiet HEAD --
+  the
     echo "-- No changes detected for the following commit, docs not updated."
     echo "https://github.com/driftyco/$CIRCLE_PROJECT_REPONAME/commit/$CIRCLE_SHA1"
   else
