ea53a1923a75230f9fae3ccaceb59d34d5b5887e
Perry Govier <perrygovier@users.noreply.github.com>
2016-12-05 17:00:05 -0600

    chore(CI): reverting benign testing change

--- src/plugins/appavailability.ts
+++ src/plugins/appavailability.ts
@@ -5,7 +5,7 @@ import { Cordova, Plugin } from './plugin';
  * @description
  * This plugin allows you to check if an app is installed on the user's device. It requires an URI Scheme (e.g. twitter://) on iOS or a Package Name (e.g com.twitter.android) on Android.
  *
- * This requires Cordova plugin: cordova-plugin-appavailability. For more info, please see the [AppAvailability plugin docs](https://github.com/ohh2ahh/AppAvailability).
+ * Requires Cordova plugin: cordova-plugin-appavailability. For more info, please see the [AppAvailability plugin docs](https://github.com/ohh2ahh/AppAvailability).
  *
  * @usage
  * ```typescript
