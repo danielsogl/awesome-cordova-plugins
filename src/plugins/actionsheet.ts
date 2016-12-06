3a8e958684c32b22076868b198d0aee7e9fcf526
Ibby <ibby93@gmail.com>
2016-12-06 08:06:47 -0500

    style(): move interface to top

--- src/plugins/actionsheet.ts
+++ src/plugins/actionsheet.ts
@@ -1,5 +1,49 @@
 import { Cordova, Plugin } from './plugin';
 
+export interface ActionSheetOptions {
+
+  /**
+   * The labels for the buttons. Uses the index x
+   */
+  buttonLabels: string[];
+
+  /**
+   * The title for the actionsheet
+   */
+  title?: string;
+
+  /**
+   * Theme to be used on Android
+   */
+  androidTheme?: number;
+
+  /**
+   * Enable a cancel on Android
+   */
+  androidEnableCancelButton?: boolean;
+
+  /**
+   * Enable a cancel on Windows Phone
+   */
+  winphoneEnableCancelButton?: boolean;
+
+  /**
+   * Add a cancel button with text
+   */
+  addCancelButtonWithLabel?: string;
+
+  /**
+   * Add a destructive button with text
+   */
+  addDestructiveButtonWithLabel?: string;
+
+  /**
+   * On an iPad, set the X,Y position
+   */
+  position?: number[];
+
+}
+
 /**
  * @name Action Sheet
  * @description
@@ -49,49 +93,5 @@ export class ActionSheet {
    */
   @Cordova()
   static hide(options?: any): Promise<any> { return; }
-
-}
-
-export interface ActionSheetOptions {
-
-  /**
-   * The labels for the buttons. Uses the index x
-   */
-  buttonLabels: string[];
-
-  /**
-   * The title for the actionsheet
-   */
-  title?: string;
-
-  /**
-   * Theme to be used on Android
-   */
-  androidTheme?: number;
-
-  /**
-   * Enable a cancel on Android
-   */
-  androidEnableCancelButton?: boolean;
-
-  /**
-   * Enable a cancel on Windows Phone
-   */
-  winphoneEnableCancelButton?: boolean;
-
-  /**
-   * Add a cancel button with text
-   */
-  addCancelButtonWithLabel?: string;
-
-  /**
-   * Add a destructive button with text
-   */
-  addDestructiveButtonWithLabel?: string;
-
-  /**
-   * On an iPad, set the X,Y position
-   */
-  position?: number[];
 
 }
