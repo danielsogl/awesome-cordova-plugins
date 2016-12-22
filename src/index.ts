943be7980287d92f7daac8bea1ac9edac204a465
倪述荣 <xueron@xueron.com>
2016-12-22 11:47:14 +0800

    add cordova-alipay-base plugin. to support Alipay payment

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ import { initAngular1 } from './ng1';
 const DEVICE_READY_TIMEOUT = 5000;
 
 declare var window;
-
 import { ActionSheet } from './plugins/actionsheet';
 import { AdMob } from './plugins/admob';
+import { Alipay } from './plugins/alipay';
 import { AndroidFingerprintAuth } from './plugins/android-fingerprint-auth';
 import { AppAvailability } from './plugins/appavailability';
 import { AppRate } from './plugins/apprate';
@@ -128,6 +128,7 @@ import { Zip } from './plugins/zip';
 export * from './plugins/3dtouch';
 export * from './plugins/actionsheet';
 export * from './plugins/admob';
+export * from './plugins/alipay';
 export * from './plugins/android-fingerprint-auth';
 export * from './plugins/appavailability';
 export * from './plugins/apprate';
@@ -252,6 +253,7 @@ export * from './plugins/zip';
 window['IonicNative'] = {
   ActionSheet,
   AdMob,
+  Alipay,
   AndroidFingerprintAuth,
   AppAvailability,
   AppRate,
