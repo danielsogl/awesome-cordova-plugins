7226bcad50fe220cf165c3c9b88caacc8fe76b6b
Max Lynch <max@drifty.com>
2017-02-07 12:40:50 -0600

    Test

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -1,1 +1,21 @@
-tsc -p tsconfig-core.json
+#tsc -p tsconfig-core.json
+
+PLUGINS="./src/@ionic-native/plugins/*"
+PLUGIN_TSCONFIG=`cat ./scripts/build/tsconfig-plugin.json.template`
+
+BUILD_TMP='.tmp'
+
+# Clean the old build dir
+rm -rf $BUILD_TMP
+# Make it again
+mkdir $BUILD_TMP
+
+# For each plugin, replace the values in tsconfig w/ the appropriate ones for this plugin
+for d in $PLUGINS ; do
+  BASE=`basename $d`
+  PLUGIN_BUILD_DIR="$BUILD_TMP/plugins/$BASE"
+  mkdir -p "$PLUGIN_BUILD_DIR"
+  NEW_TSCONFIG="$(echo "$PLUGIN_TSCONFIG" | sed "s/{{PLUGIN}}/$BASE/g")"
+  echo "$NEW_TSCONFIG" > "$PLUGIN_BUILD_DIR/tsconfig.json"
+  
+done
