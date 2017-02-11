95ffdffb5df267ade452284e52f8cef960301d26
Eric Horodyski <eric@horodyski.me>
2017-02-10 21:39:19 -0500

    Add more constructor tests.

--- test/plugins/inappbrowser.spec.ts
+++ test/plugins/inappbrowser.spec.ts
@@ -13,8 +13,18 @@ describe('InAppBrowser', () => {
 	const options: InAppBrowserOptions = { hidden: 'yes', hardwareback: 'no' };
 	let object;
 
-	it('should create an object', () => {
-		object = new InAppBrowser("http://google.com", "_self", options);
-		expect(true).toBeDefined();
+	it('should create an object using strings and InAppBrowserOptions signature', () => {
+		object = new InAppBrowser('http://google.com', '_self', options);
+		expect(object).toBeDefined();
+	});
+
+	it('should create an object using string only signature', () => {
+		object = new InAppBrowser('http://google.com', '_self', 'location=no');
+		expect(object).toBeDefined();
+	});
+
+	it('should create an object with the least amount of parameters', () => {
+		object = new InAppBrowser('http://google.com');
+		expect(object).toBeDefined();
 	});
 });
