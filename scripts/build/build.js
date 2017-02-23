6df33835854cc330eab331342334066b25137944
Ibby <ibby93@gmail.com>
2017-02-22 19:08:34 -0500

    remove auto-install and cleanup

--- scripts/build/build.js
+++ scripts/build/build.js
@@ -6,18 +6,34 @@ const fs = require('fs-extra-promise').useFs(require('fs-extra')),
 
 // Constants for the build process. Paths and JSON files templates
 const ROOT = path.resolve(path.join(__dirname, '../../')), // root ionic-native directory
-  VERSION = require(path.resolve(ROOT, 'package.json')).version, // current ionic-native version
   PLUGINS_PATH = path.resolve(ROOT, 'src/@ionic-native/plugins'), // path to plugins source files
   CORE_PACKAGE_JSON = require(path.resolve(__dirname, 'core-package.json')), // core package.json
-  UTILS_PACKAGE_JSON = require(path.resolve(__dirname, 'utils-package.json')), // utils package.json
   PLUGIN_PACKAGE_JSON = require(path.resolve(__dirname, 'plugin-package.json')), // plugin package.json template
   PLUGIN_TS_CONFIG = require(path.resolve(__dirname, 'tsconfig-plugin.json')), // plugin tsconfig template
-  PLUGIN_CONFIG = require(path.resolve(__dirname, 'plugin-config.json')), // plugin config for @ionic-native/utils
   BUILD_TMP = path.resolve(ROOT, '.tmp'), // tmp directory path
   BUILD_DIST_ROOT = path.resolve(ROOT, 'dist/packages-dist/@ionic-native'), // dist directory root path
   BUILD_PLUGINS_DIST = path.resolve(BUILD_DIST_ROOT, 'plugins'), // plugins dist directory path
-  BUILD_CORE_DIST = path.resolve(BUILD_DIST_ROOT, 'core'), // core dist directory path
-  BUILD_UTILS_DIST = path.resolve(BUILD_DIST_ROOT, 'utils');
+  BUILD_CORE_DIST = path.resolve(BUILD_DIST_ROOT, 'core'); // core dist directory path
+
+
+// dependency versions
+const ANGULAR_VERSION = '2.2.1',
+  RXJS_VERSION = '5.0.0-beta.12',
+  IONIC_NATIVE_VERSION = require(path.resolve(ROOT, 'package.json')).version;
+
+// package dependencies
+const CORE_PEER_DEPS = {
+  'rxjs': RXJS_VERSION
+};
+
+const PLUGIN_PEER_DEPS = {
+  '@ionic-native/core': IONIC_NATIVE_VERSION,
+  '@angular/core': ANGULAR_VERSION,
+  'rxjs': RXJS_VERSION
+};
+
+// set peer dependencies for all plugins
+PLUGIN_PACKAGE_JSON.peerDependencies = PLUGIN_PEER_DEPS;
 
 
 // Delete dist directory and any temporary files
@@ -33,17 +49,9 @@ fs.mkdirpSync(BUILD_TMP);
 
 // Prepare and copy the core module's package.json
 console.log('Preparing core module package.json');
-CORE_PACKAGE_JSON.version = VERSION;
-CORE_PACKAGE_JSON.dependencies = { "@ionic-native/utils": VERSION };
+CORE_PACKAGE_JSON.version = IONIC_NATIVE_VERSION;
+CORE_PACKAGE_JSON.peerDependencies = CORE_PEER_DEPS;
 fs.writeJsonSync(path.resolve(BUILD_CORE_DIST, 'package.json'), CORE_PACKAGE_JSON);
-
-console.log('Preparing utils module package.json');
-UTILS_PACKAGE_JSON.version = VERSION;
-fs.mkdirpSync(BUILD_UTILS_DIST);
-fs.writeJsonSync(path.resolve(BUILD_UTILS_DIST, 'package.json'), UTILS_PACKAGE_JSON);
-
-console.log('Copying utils module');
-fs.copySync(path.resolve(__dirname, 'utils.js'), path.resolve(BUILD_UTILS_DIST, 'index.js'));
 
 
 // Fetch a list of the plugins
@@ -81,38 +89,12 @@ const addPluginToQueue = pluginName => {
 
         return fs.writeJsonAsync(tsConfigPath, tsConfig);
       })
-      .then(() => fs.readFileAsync(PLUGIN_SRC_PATH, 'utf-8')) // read the plugin definition
-      .then((pluginFile) => {
-
-        let packageLocator,
-          installVariables,
-          regexPlugin = /plugin:\s'(.*)',?\s/g.exec(pluginFile),
-          regexPluginName = /pluginName:\s'(.*)',?\s/g.exec(pluginFile),
-          regexVars = /installVariables:\s(\[.*]),?\s/g.exec(pluginFile);
-
-        if (regexPlugin) {
-          packageLocator = regexPlugin[1];
-        }
-
-        if (regexVars) {
-          installVariables = JSON.parse(regexVars[1].replace(/'/g, '"'));
-        }
-
-        // clone plugin-config.json
-        const pluginConfig = JSON.parse(JSON.stringify(PLUGIN_CONFIG));
-
-        pluginConfig.name = regexPluginName[1];
-        pluginConfig.variables = installVariables;
-        pluginConfig.locator = packageLocator;
-
-        return fs.writeJsonAsync(path.resolve(BUILD_PLUGINS_DIST, pluginName, 'plugin-config.json'), pluginConfig);
-      })
       .then(() => {
         // clone package.json
         const packageJson = JSON.parse(JSON.stringify(PLUGIN_PACKAGE_JSON));
 
         packageJson.name = `@ionic-native/${pluginName}`;
-        packageJson.version = packageJson.peerDependencies['@ionic-native/core'] = packageJson.peerDependencies['@ionic-native/utils'] = VERSION;
+        packageJson.version = IONIC_NATIVE_VERSION;
 
         return fs.writeJsonAsync(path.resolve(BUILD_PLUGINS_DIST, pluginName, 'package.json'), packageJson);
       })
