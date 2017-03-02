d985c20d6ac835b810b1acf21d0cce8dd2a99ebe
Alex <alexongithub@gmail.com>
2017-03-02 09:37:38 -0500

    fix(health): remove unused Plugin option

--- src/plugins/health.ts
+++ src/plugins/health.ts
@@ -169,8 +169,7 @@ export interface HealthData {
   pluginName: 'Health',
   plugin: 'cordova-plugin-health',
   pluginRef: 'navigator.health',
-  repo: 'https://github.com/dariosalvi78/cordova-plugin-health',
-  install: ''
+  repo: 'https://github.com/dariosalvi78/cordova-plugin-health'
 })
 export class Health {
 
