0c2e84c9c01462c66f0a5b640f7c240da627f0d6
Ibby <ibby93@gmail.com>
2017-03-16 16:39:59 -0400

    refactor(paypal): PayPal is brand name

--- src/@ionic-native/plugins/paypal/index.ts
+++ src/@ionic-native/plugins/paypal/index.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Plugin, Cordova } from '@ionic-native/core';
 /**
- * @name Pay Pal
+ * @name PayPal
  * @description
  * PayPal plugin for Cordova/Ionic Applications
  *
