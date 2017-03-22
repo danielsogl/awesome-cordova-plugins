873ee71c821ca9e2a2e529f3cfce2099ca0eee8d
joshbabb <joshua@babb.host>
2017-03-21 21:32:12 -0500

    import fixes
    (no-unused-variable) @ionic-native/plugins/jins-meme/index.ts[1, 10]:
    Unused import: 'Injectable'
    
    stream.js:74
    throw er; // Unhandled stream error in pipe.
    ^
    Error: Failed to lint: @ionic-native/plugins/jins-meme/index.ts [1, 10]:
    Unused import: 'Injectable'.

--- src/@ionic-native/plugins/jins-meme/index.ts
+++ src/@ionic-native/plugins/jins-meme/index.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@angular/core';
+// import { Injectable } from '@angular/core';
 import { Cordova, Plugin } from '@ionic-native/core';
+import { Observable } from 'rxjs/Observable';
 
 declare var cordova: any;
 
