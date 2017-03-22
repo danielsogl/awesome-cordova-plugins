09bc6cf9a39369cb085c34aece4c8e9185d0ce05
joshbabb <joshua@babb.host>
2017-03-22 01:08:31 -0500

    ihadeed's requested changed
    see
    https://github.com/driftyco/ionic-native/pull/1212#pullrequestreview-28298382

--- src/@ionic-native/plugins/jins-meme/index.ts
+++ src/@ionic-native/plugins/jins-meme/index.ts
@@ -1,4 +1,5 @@
-import { Cordova, Plugin } from '@ionic-native/core';
+import { Injectable } from '@angular/core';
+import { Plugin, Cordova, CordovaCheck } from '@ionic-native/core';
 import { Observable } from 'rxjs/Observable';
 
 declare var cordova: any;
@@ -10,160 +11,148 @@ declare var cordova: any;
  *
  * @usage
  * ```
- * import { JinsMeme } from 'ionic-native';
- *
- * JinsMeme.functionName('Hello', 123)
- *   .then((something: any) => doSomething(something))
- *   .catch((error: any) => console.log(error));
+ * import { JinsMeme } from '@ionic-native/jins-meme';
+ * 
+ * constructor(private jinsMeme: JinsMeme) { }
+ * 
+ * ...
+ * 
+ * this.jinsMeme.functionName()
+ *   .then(...)
+ *   .catch(...);
  *
  * ```
  */
 @Plugin({
-  pluginName: 'JinsMeme',
-  plugin: 'JinsMemeSDK-Plugin-Cordova', // npm package name, example: cordova-plugin-camera
-  pluginRef: 'cordova.plugins.JinsMemePlugin', // the variable reference to call the plugin, example: navigator.geolocation
-  repo: 'https://github.com/jins-meme/JinsMemeSDK-Plugin-Cordova.git', // the github repository URL for the plugin
-  install: '' // OPTIONAL install command, in case the plugin requires variables
+  pluginName: 'Jins Meme',
+  plugin: 'JinsMemeSDK-Plugin-Cordova',
+  pluginRef: 'cordova.plugins.JinsMemePlugin',
+  repo: 'https://github.com/jins-meme/JinsMemeSDK-Plugin-Cordova.git'
 })
 @Injectable()
 export class JinsMeme {
-
   /**
-  * Authentication and authorization of App and SDK.
-  * Must call this method at first.
-  *
-  *@param {string} setAppClientID
-  *@param {string} clientSecret
-  *@returns {Promise<any>}
-  */
+   * Authentication and authorization of App and SDK.
+   * Must call this method at first.
+   *
+   *@param {string} setAppClientID
+   *@param {string} clientSecret
+   *@returns {Promise<any>}
+   */
   @Cordova()
-  static setAppClientID(appClientId: string, clientSecret: string): Promise<any> { return; }
-
+  setAppClientID(appClientId: string, clientSecret: string): Promise<any> { return; }
   /**
-  * Starts scanning for JINS MEME.
-  * @returns {Observable<any>}
-  */
+   * Starts scanning for JINS MEME.
+   * @returns {Observable<any>}
+   */
   @Cordova({
     observable: true,
     clearFunction: 'stopScan',
     clearWithArgs: true
   })
-  static startScan(): Observable<any> { return; }
-
+  startScan(): Observable<any> { return; }
   /**
    * Stops scanning JINS MEME.
    * @returns {Promise<any>}
    */
   @Cordova()
-  static stopScan(): Promise<any> { return; }
-
+  stopScan(): Promise<any> { return; }
   /**
-  * Establishes connection to JINS MEME.
-  * @param {string} target
-  */
-  // @Cordova({observable: true})
-  static connect(target: string): Observable<any> {
-     return new Observable<any> ((observer: any) => {
+   * Establishes connection to JINS MEME.
+   * @param {string} target
+   * @returns {Observable<any>}
+   */
+  @CordovaCheck({
+    observable: true
+  })
+  connect(target: string): Observable<any> {
+     return new Observable<any>((observer: any) => {
        let data = cordova.plugins.JinsMemePlugin.connect(target, observer.next.bind(observer), observer.complete.bind(observer), observer.error.bind(observer));
        return () => console.log(data);
      });
     }
-
   /**
-   * Set auto connection.
+   * Set auto connection mode.
    *@param {Boolean} flag
    *@returns {Promise<any>}
    */
   @Cordova()
-  static setAutoConnect(flag: boolean): Promise<any> { return; }
-
+  setAutoConnect(flag: boolean): Promise<any> { return; }
   /**
-  * Returns whether a connection to JINS MEME has been established.
-  *@returns {Promise<any>}
-  */
+   * Returns whether a connection to JINS MEME has been established.
+   *@returns {Promise<any>}
+   */
   @Cordova()
-  static isConnected(): Promise<any> { return; }
-
+  isConnected(): Promise<any> { return; }
   /**
    * Disconnects from JINS MEME.
    *@returns {Promise<any>}
    */
   @Cordova()
-  static disconnect(): Promise<any> { return; }
-
+  disconnect(): Promise<any> { return; }
   /**
- * Starts receiving realtime data.
- * @returns {Observable<any>}
- */
- @Cordova({
-   observable: true,
-   clearFunction: 'stopDataReport',
-   clearWithArgs: true
- })
- static startDataReport(): Observable<any> { return; }
-
-  /**
- * Stops receiving data.
- *@returns {Promise<any>}
- */
+   * Starts receiving realtime data.
+   * @returns {Observable<any>}
+   */
+  @Cordova({
+    observable: true,
+    clearFunction: 'stopDataReport',
+    clearWithArgs: true
+  })
+  startDataReport(): Observable<any> { return; }
+   /**
+   * Stops receiving data.
+   *@returns {Promise<any>}
+   */
   @Cordova()
-  static stopDataReport(): Promise<any> { return; }
-
+  stopDataReport(): Promise<any> { return; }
   /**
-  * Returns SDK version.
-  *
-  *@returns {Promise<any>}
-  */
+   * Returns SDK version.
+   *
+   *@returns {Promise<any>}
+   */
   @Cordova()
-  static getSDKVersion(): Promise<any> { return; }
-
+  getSDKVersion(): Promise<any> { return; }
   /**
-  * Returns JINS MEME connected with other apps.
-  *@returns {Promise<any>}
-  */
+   * Returns JINS MEME connected with other apps.
+   *@returns {Promise<any>}
+   */
   @Cordova()
-  static getConnectedByOthers(): Promise<any> { return; }
-
-
+  getConnectedByOthers(): Promise<any> { return; }
   /**
-  * Returns calibration status
-  *@returns {Promise<any>}
-  */
+   * Returns calibration status
+   *@returns {Promise<any>}
+   */
   @Cordova()
-  static isCalibrated(): Promise<any> { return; }
-
+  isCalibrated(): Promise<any> { return; }
   /**
-  * Returns device type.
-  *@returns {Promise<any>}
-  */
+   * Returns device type.
+   *@returns {Promise<any>}
+   */
   @Cordova()
-  static getConnectedDeviceType(): Promise<any> { return; }
-
+  getConnectedDeviceType(): Promise<any> { return; }
   /**
-  * Returns hardware version.
-  *@returns {Promise<any>}
-  */
+   * Returns hardware version.
+   *@returns {Promise<any>}
+   */
   @Cordova()
-  static getConnectedDeviceSubType(): Promise<any> { return; }
-
+  getConnectedDeviceSubType(): Promise<any> { return; }
   /**
-  * Returns FW Version.
-  *@returns {Promise<any>}
-  */
+   * Returns FW Version.
+   *@returns {Promise<any>}
+   */
   @Cordova()
-  static getFWVersion(): Promise<any> { return; }
-
+  getFWVersion(): Promise<any> { return; }
   /**
-  * Returns HW Version.
-  *@returns {Promise<any>}
-  */
+   * Returns HW Version.
+   *@returns {Promise<any>}
+   */
   @Cordova()
-  static getHWVersion(): Promise<any> { return; }
-
+  getHWVersion(): Promise<any> { return; }
   /**
-  * Returns response about whether data was received or not.
-  *@returns {Promise<any>}
-  */
+   * Returns response about whether data was received or not.
+   *@returns {Promise<any>}
+   */
   @Cordova()
-  static isDataReceiving(): Promise<any> { return; }
+  isDataReceiving(): Promise<any> { return; }
 }
