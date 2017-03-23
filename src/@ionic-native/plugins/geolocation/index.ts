957c0c1122d051c8c3aedeb66f12110805ac1a83
Juan Antonio Gómez Cabo <gomezcabo@gmail.com>
2017-03-23 15:53:39 +0000

    Update index.ts
    The import statement was not working properly. With the change I propose, the issue is solved.
    
    Thanks :)

--- src/@ionic-native/plugins/geolocation/index.ts
+++ src/@ionic-native/plugins/geolocation/index.ts
@@ -120,7 +120,7 @@ export interface GeolocationOptions {
  * @usage
  *
  * ```typescript
- * import { Geolocation } from '@ionic-native/geolocation';
+ * import { Geolocation } from 'ionic-native';
  *
  * ...
  *
