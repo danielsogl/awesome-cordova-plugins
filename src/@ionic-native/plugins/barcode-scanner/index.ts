2f7a6fedb8e6aee5715070a72b227fe597c6c81d
Daniel Sogl <MTechDE@users.noreply.github.com>
2017-04-30 19:22:57 +0200

    typo(barcode-scanner): fixe circle lint error

--- src/@ionic-native/plugins/barcode-scanner/index.ts
+++ src/@ionic-native/plugins/barcode-scanner/index.ts
@@ -57,7 +57,7 @@ export interface BarcodeScannerOptions {
 }
 
 export interface BarcodeScanResult {
-  format: "QR_CODE" | "DATA_MATRIX" | "UPC_E" | "UPC_A" | "EAN_8" | "EAN_13" | "CODE_128" | "CODE_39" | "CODE_93" | "CODABAR" | "ITF" | "RSS14" | "RSS_EXPANDED" | "PDF417" | "AZTEC" | "MSI";
+  format: 'QR_CODE' | 'DATA_MATRIX' | 'UPC_E' | 'UPC_A' | 'EAN_8' | 'EAN_13' | 'CODE_128' | 'CODE_39' | 'CODE_93' | 'CODABAR' | 'ITF' | 'RSS14' | 'RSS_EXPANDED' | 'PDF417' | 'AZTEC' | 'MSI';
   cancelled: boolean;
   text: string;
 }
