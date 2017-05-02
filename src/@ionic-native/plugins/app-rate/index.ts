fe91624647f3ff9901115d156e61f32a4f304c6e
Daniel Sogl <MTechDE@users.noreply.github.com>
2017-05-02 22:59:21 +0200

    Update index.ts

--- src/@ionic-native/plugins/app-rate/index.ts
+++ src/@ionic-native/plugins/app-rate/index.ts
@@ -111,12 +111,12 @@ export interface AppUrls {
  *
  * ...
  *
- *  this.appRate.preferences.storeAppURL = {
- *    usesUntilPrompt: 3,
- *    ios: '<my_app_id>',
- *    android: 'market://details?id=<package_name>',
- *    windows: 'ms-windows-store://review/?ProductId=<Store_ID>'
- *  };
+ * this.appRate.preferences.storeAppURL = {
+ *   usesUntilPrompt: 3,
+ *   ios: '<my_app_id>',
+ *   android: 'market://details?id=<package_name>',
+ *   windows: 'ms-windows-store://review/?ProductId=<Store_ID>'
+ * };
  *
  * this.appRate.promptForRating(false);
  * ```
