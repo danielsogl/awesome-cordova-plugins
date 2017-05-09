70c9558d2bf9a819f0c7810af730e83524d185fb
Daniel Sogl <daniel@sogls.de>
2017-05-09 23:32:47 +0200

    docs(youtube-video-player): update repo (#1496)
    * refactor(YoutubeVideoPlayer): updated repo
    
    fix #1352
    
    * Update index.ts
    
    * Update index.ts
    
    * Update index.ts

--- src/@ionic-native/plugins/youtube-video-player/index.ts
+++ src/@ionic-native/plugins/youtube-video-player/index.ts
@@ -6,6 +6,13 @@ import { Plugin, Cordova, IonicNativePlugin } from '@ionic-native/core';
  * Plays YouTube videos in Native YouTube App
  *
  * @usage
+ * For Android 5.0+ you will need to add the following to config.xml
+ * ```xml
+ * <preference name="YouTubeDataApiKey" value="[YOUR YOUTUBE API]" />
+ * ```
+ * For more information: https://developers.google.com/youtube/v3/getting-started
+ *
+ *
  * ```typescript
  * import { YoutubeVideoPlayer } from '@ionic-native/youtube-video-player';
  *
@@ -20,9 +27,9 @@ import { Plugin, Cordova, IonicNativePlugin } from '@ionic-native/core';
  */
 @Plugin({
   pluginName: 'YoutubeVideoPlayer',
-  plugin: 'https://github.com/Glitchbone/CordovaYoutubeVideoPlayer.git',
+  plugin: 'https://github.com/JonSmart/CordovaYoutubeVideoPlayer',
   pluginRef: 'YoutubeVideoPlayer',
-  repo: 'https://github.com/Glitchbone/CordovaYoutubeVideoPlayer',
+  repo: 'https://github.com/JonSmart/CordovaYoutubeVideoPlayer',
   platforms: ['Android', 'iOS']
 })
 @Injectable()
