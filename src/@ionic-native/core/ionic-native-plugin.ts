b1f80c62007dd0e2c989c538893477eec50add00
Ibby Hadeed <ibby93@gmail.com>
2017-05-12 11:44:50 -0400

    remove unecessary code

--- src/@ionic-native/core/ionic-native-plugin.ts
+++ src/@ionic-native/core/ionic-native-plugin.ts
@@ -3,45 +3,33 @@ import { getPlugin } from './util';
 
 export class IonicNativePlugin {
 
-  protected static _config: PluginConfig;
-
   private static _class: any;
 
   /**
    * Returns a boolean that indicates whether the plugin is installed
    * @return {boolean}
    */
-  static installed(): boolean {
-    return !!getPlugin(this._config.pluginRef);
-  }
+  static installed(): boolean { return; }
 
   /**
    * Returns the original plugin object
    */
-  static getPlugin(): any {
-    return getPlugin(this._config.pluginRef);
-  }
+  static getPlugin(): any { return; }
 
   /**
    * Returns the plugin's name
    */
-  static getPluginName(): string {
-    return this._config.pluginName;
-  }
+  static getPluginName(): string { return; }
 
   /**
    * Returns the plugin's reference
    */
-  static getPluginRef(): string {
-    return this._config.pluginRef;
-  }
+  static getPluginRef(): string { return; }
 
   /**
    * Returns the plugin's install name
    */
-  static getPluginInstallName(): string {
-    return this._config.plugin;
-  }
+  static getPluginInstallName(): string { return; }
 
   static useClass(_class: any): void {
 
