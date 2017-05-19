27bb006eb2f460bc5c902be1e78daf8e014c3f60
Jonathan Alexander <jonathan@jalexanderconsulting.com>
2017-05-19 19:57:54 -0400

    Update index.ts
    The install text is missing cordova so it won't install just by copying and pasting in a terminal.

--- src/@ionic-native/plugins/deeplinks/index.ts
+++ src/@ionic-native/plugins/deeplinks/index.ts
@@ -79,7 +79,7 @@ export interface DeeplinkMatch {
   pluginRef: 'IonicDeeplink',
   repo: 'https://github.com/driftyco/ionic-plugin-deeplinks',
   platforms: ['iOS', 'Android', 'Browser'],
-  install: 'ionic plugin add ionic-plugin-deeplinks --variable URL_SCHEME=myapp --variable DEEPLINK_SCHEME=https --variable DEEPLINK_HOST=example.com --variable ANDROID_PATH_PREFIX=/',
+  install: 'ionic cordova plugin add ionic-plugin-deeplinks --variable URL_SCHEME=myapp --variable DEEPLINK_SCHEME=https --variable DEEPLINK_HOST=example.com --variable ANDROID_PATH_PREFIX=/',
   installVariables: ['URL_SCHEME', 'DEEPLINK_SCHEME', 'DEEPLINK_HOST', 'ANDROID_PATH_PREFIX']
 })
 @Injectable()
