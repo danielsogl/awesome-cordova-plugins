d9546f15edbc6bd53610954a92e8e05e407ec361
Niklas Häusele <niklas.haeusele@googlemail.com>
2017-06-01 19:06:07 +0200

    docs(): fix typo (#1632)

--- src/@ionic-native/plugins/video-player/index.ts
+++ src/@ionic-native/plugins/video-player/index.ts
@@ -20,7 +20,7 @@ export interface VideoOptions {
 /**
  * @name Video Player
  * @description
- * A Codova plugin that simply allows you to immediately play a video in fullscreen mode.
+ * A Cordova plugin that simply allows you to immediately play a video in fullscreen mode.
  *
  * Requires Cordova plugin: `com.moust.cordova.videoplayer`. For more info, please see the [VideoPlayer plugin docs](https://github.com/moust/cordova-plugin-videoplayer).
  *
