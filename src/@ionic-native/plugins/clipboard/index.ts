9950b18e3817c31503d8e4d1f3e73f444b852edb
Daniel Sogl <daniel@sogls.de>
2017-06-03 19:25:10 +0200

    refactor(clipboard): changed plugin source (#1643)
    * refactor(tts): changed plugin source
    
    * Update index.ts
    
    * Update index.ts

--- src/@ionic-native/plugins/clipboard/index.ts
+++ src/@ionic-native/plugins/clipboard/index.ts
@@ -31,10 +31,10 @@ import { Cordova, Plugin, IonicNativePlugin } from '@ionic-native/core';
  */
 @Plugin({
   pluginName: 'Clipboard',
-  plugin: 'https://github.com/VersoSolutions/CordovaClipboard.git',
+  plugin: 'danielsogl-cordova-plugin-clipboard',
   pluginRef: 'cordova.plugins.clipboard',
-  repo: 'https://github.com/VersoSolutions/CordovaClipboard',
-  platforms: ['Android', 'iOS', 'Windows Phone 8']
+  repo: 'https://github.com/danielsogl/cordova-plugin-clipboard',
+  platforms: ['Android', 'iOS', 'Windows', 'Windows Phone 8']
 })
 @Injectable()
 export class Clipboard extends IonicNativePlugin {
