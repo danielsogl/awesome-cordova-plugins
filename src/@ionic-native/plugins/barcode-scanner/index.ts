3a9a93300dc131cdb8d5ad875eaa647417cd2f96
haui <psi.haui@gmail.com>
2017-07-13 23:48:17 +0200

    Added ASSUME_GS1 parameter
    Added ASSUME_GS1 to active proper FNC detection (Application Identifier with variable field length) is GS1 Barcodes

--- src/@ionic-native/plugins/barcode-scanner/index.ts
+++ src/@ionic-native/plugins/barcode-scanner/index.ts
@@ -52,7 +52,12 @@ export interface BarcodeScannerOptions {
    * Display scanned text for X ms. 0 suppresses it entirely, default 1500. Supported on Android only.
    */
   resultDisplayDuration?: number;
-
+  
+  /*
+  * Active proper FNC detection for GS1 Barcodes
+  */
+  ASSUME_GS1?: boolean;
+  
 }
 
 export interface BarcodeScanResult {
