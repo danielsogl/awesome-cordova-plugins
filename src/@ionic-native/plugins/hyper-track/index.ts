c63e30be4a20451094555a579d9f25ece658c112
Daniel Olivares <danieloddl2@hotmail.com>
2017-07-16 18:01:18 -0500

    feat(HyperTrack): add completeAction

--- src/@ionic-native/plugins/hyper-track/index.ts
+++ src/@ionic-native/plugins/hyper-track/index.ts
@@ -124,6 +124,23 @@ export class HyperTrack extends IonicNativePlugin {
   createAndAssignAction(type: String, lookupId: String, expectedPlaceAddress: String, expectedPlaceLatitude: Number, expectedPlaceLongitude: Number): Promise<any> { return; }
 
   /**
+   * Complete an action from the SDK by its ID
+   *
+   * @usage
+   * ```
+   * HyperTrack.completeAction('action-id').then(() => {
+   *    // handle success
+   *  }, error => {
+   *    // handle error
+   *  });
+   * ```
+   * @see {@link https://docs.hypertrack.com/sdks/cordova/reference.html#methods|Hypertrack Cordova Methods Reference}
+   * @returns {Promise<any>}
+   */
+  @Cordova()
+  completeAction(id: String): Promise<any> { return; }
+
+  /**
    * Disable the SDK and stop tracking.
    * Needs user setting (via getOrCreateUser or setUserId).
    *
