18843c13e5f468094a3cf541d6182d74118151ef
masashiGMS <masashikatsumata.gms@gmail.com>
2017-07-24 11:34:01 -0700

    Bug fix: example code was wrong.

--- src/@ionic-native/plugins/google-maps/index.ts
+++ src/@ionic-native/plugins/google-maps/index.ts
@@ -381,75 +381,76 @@ export const GoogleMapsMapTypeId: { [mapType: string]: MapType; } = {
  *  GoogleMaps,
  *  GoogleMap,
  *  GoogleMapsEvent,
- *  LatLng,
+ *  GoogleMapOptions,
  *  CameraPosition,
  *  MarkerOptions,
  *  Marker
  * } from '@ionic-native/google-maps';
+ * import { Component } from "@angular/core/";
  *
  * import { Platform } from 'ionic-angular';
+ * @Component({
+ *   selector: 'page-home',
+ *   templateUrl: 'home.html'
+ * })
+ * export class HomePage {
+ *   map: GoogleMap;
+ *   mapElement: HTMLElement;
+ *   constructor(private googleMaps: GoogleMaps, public platform: Platform) {
  *
- * export class MapPage {
- *  constructor(private googleMaps: GoogleMaps, public platform: Platform) {
- *
- *    // Wait the native plugin is ready.
- *    platform.ready().then(() => {
- *      this.loadMap();
- *    });
- *  }
- *
- * // Don't use the ngAfterViewInit(). The native plugin is not ready.
- * //ngAfterViewInit() {
- * //  this.loadMap();
- * //}
- *
- * loadMap() {
- *  // make sure to create following structure in your view.html file
- *  // and add a height (for example 100%) to it, else the map won't be visible
- *  // <ion-content>
- *  //  <div #map id="map" style="height:100%;"></div>
- *  // </ion-content>
- *
- *  // create a new map by passing HTMLElement
- *  let element: HTMLElement = document.getElementById('map');
- *
- *  let map: GoogleMap = this.googleMaps.create(element);
- *
- *  // listen to MAP_READY event
- *  // You must wait for this event to fire before adding something to the map or modifying it in anyway
- *  map.one(GoogleMapsEvent.MAP_READY).then(
- *    () => {
- *      console.log('Map is ready!');
- *      // Now you can add elements to the map like the marker
- *    }
- *  );
- *
- *  // create CameraPosition
- *  let position: CameraPosition = {
- *    target: {
- *      lat: 43.0741904,
- *      lng: -89.3809802
- *    },
- *    zoom: 18,
- *    tilt: 30
- *  };
- *
- *  // move the map's camera to position
- *  map.moveCamera(position);
- *
- *  // create new marker
- *  let markerOptions: MarkerOptions = {
- *    position: ionic,
- *    title: 'Ionic'
- *  };
- *
- *  const marker: Marker = map.addMarker(markerOptions)
- *    .then((marker: Marker) => {
- *       marker.showInfoWindow();
+ *     // Wait the native plugin is ready.
+ *     platform.ready().then(() => {
+ *       this.loadMap();
  *     });
- *  }
+ *   }
  *
+ *   // Don't use the ngAfterViewInit(). The native plugin is not ready.
+ *   //ngAfterViewInit() {
+ *   // this.loadMap();
+ *   //}
+ *
+ *  loadMap() {
+ *     this.mapElement = document.getElementById('map');
+ *
+ *     let mapOptions: GoogleMapOptions = {
+ *       camera: {
+ *         target: {
+ *           lat: 43.0741904,
+ *           lng: -89.3809802
+ *         },
+ *         zoom: 18,
+ *         tilt: 30
+ *       }
+ *     };
+ *
+ *     this.map = this.googleMaps.create(this.mapElement, mapOptions);
+ *
+ *     // Wait the MAP_READY before using any methods.
+ *     this.map.one(GoogleMapsEvent.MAP_READY)
+ *       .then(() => {
+ *         console.log('Map is ready!');
+ *
+ *         // Now you can use all methods safely.
+ *         this.map.addMarker({
+ *             title: 'Ionic',
+ *             icon: 'blue',
+ *             animation: 'DROP',
+ *             position: {
+ *               lat: 43.0741904,
+ *               lng: -89.3809802
+ *             }
+ *           })
+ *           .then(marker => {
+ *             marker.on(GoogleMapsEvent.MARKER_CLICK)
+ *               .subscribe(() => {
+ *                 alert('clicked');
+ *               });
+ *           });
+ *
+ *       });
+ *   }
  * }
+ * 
  * ```
  * @classes
  * GoogleMap
