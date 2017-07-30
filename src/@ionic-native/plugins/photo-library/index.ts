e964fa23983d78a719a48bbe5c5f3d028158014d
Tom Sundstrom <office@tomsun.ax>
2017-07-30 23:18:42 +0300

    Move error message to the error callback

--- src/@ionic-native/plugins/photo-library/index.ts
+++ src/@ionic-native/plugins/photo-library/index.ts
@@ -31,8 +31,8 @@ import { Injectable } from '@angular/core';
  *         console.log(libraryItem.albumIds);    // array of ids of appropriate AlbumItem, only of includeAlbumsData was used
  *       });
  *     },
- *     error: err => {},
- *     complete: () => { console.log('could not get photos'); }
+ *     error: err => { console.log('could not get photos'); },
+ *     complete: () => { }
  *   });
  * })
  * .catch(err => console.log('permissions weren\'t granted'));
