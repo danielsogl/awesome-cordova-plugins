591386e734c0861d8977368091eceb07737aa5a0
wf9a5m75 <wf9a5m75@gmail.com>
2017-08-08 15:54:48 -0700

    update: event names

--- src/@ionic-native/plugins/google-maps/index.ts
+++ src/@ionic-native/plugins/google-maps/index.ts
@@ -331,6 +331,7 @@ export interface VisibleRegion {
  */
 export const GoogleMapsEvent: { [eventName: string]: string; } = {
   MAP_READY: 'map_ready',
+  MAP_LOADED: 'map_loaded',
   MAP_CLICK: 'map_click',
   MAP_LONG_CLICK: 'map_long_click',
   MY_LOCATION_BUTTON_CLICK: 'my_location_button_click',
@@ -339,18 +340,23 @@ export const GoogleMapsEvent: { [eventName: string]: string; } = {
   CAMERA_MOVE_START: 'camera_move_start',
   CAMERA_MOVE: 'camera_move',
   CAMERA_MOVE_END: 'camera_move_end',
+  OVERLAY_CLICK: 'overlay_click',
   POLYGON_CLICK: 'polygon_click',
   POLYLINE_CLICK: 'polyline_click',
   CIRCLE_CLICK: 'circle_click',
-  GROUND_OVERLAY_CLICK: 'ground_overlay_click',
+  GROUND_OVERLAY_CLICK: 'groundoverlay_click',
   INFO_CLICK: 'info_click',
   INFO_LONG_CLICK: 'info_long_click',
   INFO_CLOSE: 'info_close',
   INFO_OPEN: 'info_open',
+  CLUSTER_CLICK: 'cluster_click',
   MARKER_CLICK: 'marker_click',
   MARKER_DRAG: 'marker_drag',
   MARKER_DRAG_START: 'marker_drag_start',
-  MARKER_DRAG_END: 'marker_drag_end'
+  MARKER_DRAG_END: 'marker_drag_end',
+  MAP_DRAG: 'map_drag',
+  MAP_DRAG_START: 'map_drag_start',
+  MAP_DRAG_END: 'map_drag_end'
 };
 
 /**
