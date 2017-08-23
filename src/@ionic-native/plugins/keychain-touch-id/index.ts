fe02c84fd987507dfb9508edf576eae3e3fcf3cd
Ibby Hadeed <ibby93@gmail.com>
2017-08-23 19:34:55 -0400

    clean up

--- src/@ionic-native/plugins/keychain-touch-id/index.ts
+++ src/@ionic-native/plugins/keychain-touch-id/index.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Plugin, Cordova,  IonicNativePlugin } from '@ionic-native/core';
+import { Plugin, Cordova, IonicNativePlugin } from '@ionic-native/core';
 
 /**
  * @name Keychain Touch Id
@@ -35,7 +35,6 @@ export class KeychainTouchId extends IonicNativePlugin {
 
   /**
    * Check if Touch ID / Fingerprint is supported by the device
-   *
    * @return {Promise<any>} Returns a promise that resolves when there is hardware support
    */
   @Cordova()
@@ -46,61 +45,44 @@ export class KeychainTouchId extends IonicNativePlugin {
   /**
    * Encrypts and Saves a password under the key in the device keychain, which can be retrieved after
    * successful authentication using fingerprint
-   *
    * @param key {string} the key you want to store
    * @param password {string} the password you want to encrypt and store
    * @return {Promise<any>} Returns a promise that resolves when there is a result
    */
   @Cordova()
-  save(key: string, passwrod: string): Promise<any> {
-    return;
-  }
+  save(key: string, password: string): Promise<any> { return; }
 
   /**
    * Opens the fingerprint dialog, for the given key, showing an additional message. Promise will resolve
    * with the password stored in keychain or will resolve an error code, where -1 indicated not available.
-   *
    * @param key {string} the key you want to retrieve from keychain
    * @param message {string} a message to the user
    * @return {Promise<any>} Returns a promise that resolves when the key value is successfully retrieved or an error
    */
   @Cordova()
-  verify(key: string, message: string): Promise<any> {
-    return;
-  }
+  verify(key: string, message: string): Promise<any> { return; }
 
   /**
    * Checks if there is a password stored within the keychain for the given key.
-   *
    * @param key {string} the key you want to check from keychain
-   * @return {Promise<any>} Returns a promise that resolves with success if the key
-   * is available or failure if key is not
+   * @return {Promise<any>} Returns a promise that resolves with success if the key is available or failure if key is not.
    */
   @Cordova()
-  has(key: string): Promise<any> {
-    return;
-  }
+  has(key: string): Promise<any> { return; }
 
   /**
    * Deletes the password stored under given key from the keychain.
-   *
    * @param key {string} the key you want to delete from keychain
-   * @return {Promise<any>} Returns a promise that resolves with success if the key
-   * is deleted or failure if key is not
+   * @return {Promise<any>} Returns a promise that resolves with success if the key is deleted or failure if key is not
    */
   @Cordova()
-  delete(key: string): Promise<any> {
-    return;
-  }
+  delete(key: string): Promise<any> { return; }
 
   /**
    * Sets the language of the fingerprint dialog
-   *
-   * @param locale {string} locale subtag from this list
-   * https://www.iana.org/assignments/language-subtag-registry/language-subtag-registry
-   *
+   * @param locale {string} locale subtag from [this list](https://www.iana.org/assignments/language-subtag-registry/language-subtag-registry).
    */
   @Cordova()
-  setLocale(locale: string): void { }
+  setLocale(locale: string): void {}
 
 }
