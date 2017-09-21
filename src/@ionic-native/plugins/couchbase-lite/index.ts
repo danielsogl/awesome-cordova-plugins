2ead34b94fb2e91007d8afca2ddb2516970910c0
Sandip Gadekar <sgadekar81@gmail.com>
2017-09-21 19:55:34 +0530

    whitespaces added

--- src/@ionic-native/plugins/couchbase-lite/index.ts
+++ src/@ionic-native/plugins/couchbase-lite/index.ts
@@ -4,52 +4,52 @@ import { CouchbaseLite } from '@ionic-native/couchbase-lite';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 @Injectable()
-export class LocalDatabaseService{
+export class LocalDatabaseService {
   url:string;
   constructor(
         private couchbase: CouchbaseLite,
         private platform:Platform,
-        private _http:Http){
+        private _http:Http) {
             this.initMethode();
   }
-  initMethode(){
-    this.couchbase.getURL().then((url)=>{
+  initMethode() {
+    this.couchbase.getURL().then((url)=> {
         this.url = url;
     })
   }
-  getUrl(){
+  getUrl() {
     return this.url;
   }
   /* DATABASE RELATED METHODS */
     /* create database */
-  createDatabase(database_name:string){
+  createDatabase(database_name:string) {
     let url = this.getUrl();
     url = url+database_name;
     return this._http
         .put(url)
-        .map(data => {this.results = data['results']})
-        .catch((error:any) =>{
+        .map(data => { this.results = data['results'] })
+        .catch((error:any) => {
             return Observable.throw(error.json() || 'Couchbase Lite error');
         })
   }
     /* delete database */
-  deleteDatabase(database_name:string){
+  deleteDatabase(database_name:string) {
     let url = this.getUrl();
     url = url+database_name;
     return this._http
         .delete(url)
-        .map(data => {this.results = data['results']})
-        .catch((error:any) =>{
+        .map(data => { this.results = data['results'] })
+        .catch((error:any) => {
             return Observable.throw(error.json() || 'Couchbase Lite error');
         })
   }
     /* get all database names */
-  getAllDbs(){
+  getAllDbs() {
     let url = this.getUrl();
     url = url+'_all_dbs';
     return this._http
         .get(url)
-        .map(data => {this.results = data['results']})
+        .map(data => { this.results = data['results'] })
         .catch((error:any) => {
             return Observable.throw(error.json() || 'Couchbase Lite error');
         })
