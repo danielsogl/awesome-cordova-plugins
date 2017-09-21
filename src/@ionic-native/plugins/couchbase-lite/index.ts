f605a824fe71971dacff1af03886db8b5d0173dc
Sandip Gadekar <sgadekar81@gmail.com>
2017-09-21 17:36:31 +0530

    create database method added

--- src/@ionic-native/plugins/couchbase-lite/index.ts
+++ src/@ionic-native/plugins/couchbase-lite/index.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { CouchbaseLite } from '@ionic-native/couchbase-lite';
+import { Http, URLSearchParams } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class LocalDatabaseService{
   url:string;
   constructor(
         private couchbase: CouchbaseLite,
-        private platform:Platform){
+        private platform:Platform,
+        private _http:Http){
             this.initMethode();
   }
   initMethode(){
@@ -16,5 +19,16 @@ export class LocalDatabaseService{
   }
   getUrl(){
     return this.url;
+  }
+  //   DATABASE RELATED METHODS
+  createDatabase(database_name:string){
+    let url = this.getUrl();
+    let urlWithDatabaseName = url+database_name;
+    return this._http
+        .put(url)
+        .map(data => {this.results = data['results']})
+        .catch((error:any) =>{
+            return Observable.throw(error.json() || 'Couchbase Lite error');
+        })
   }
 }
