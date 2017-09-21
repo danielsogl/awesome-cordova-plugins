032741a98723349814ab67faa71015729a168caa
Sandip Gadekar <sgadekar81@gmail.com>
2017-09-21 17:22:39 +0530

    added get url method

--- src/@ionic-native/plugins/couchbase-lite/index.ts
+++ src/@ionic-native/plugins/couchbase-lite/index.ts
@@ -1,1 +1,20 @@
-
+import { Injectable } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { CouchbaseLite } from '@ionic-native/couchbase-lite';
+@Injectable()
+export class LocalDatabaseService{
+  url:string;
+  constructor(
+        private couchbase: CouchbaseLite,
+        private platform:Platform){
+            this.initMethode();
+  }
+  initMethode(){
+    this.couchbase.getURL().then((url)=>{
+        this.url = url;
+    })
+  }
+  getUrl(){
+    return this.url;
+  }
+}
