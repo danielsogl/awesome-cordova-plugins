fbe7fc5c03e05f3166165fd19172937141e6e518
Sandip Gadekar <sgadekar81@gmail.com>
2017-09-22 09:56:04 +0530

    usage added in commented section

--- src/@ionic-native/plugins/couchbase-lite/index.ts
+++ src/@ionic-native/plugins/couchbase-lite/index.ts
@@ -1,57 +1,84 @@
+import { Plugin, Cordova, IonicNativePlugin } from '@ionic-native/core';
 import { Injectable } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { CouchbaseLite } from '@ionic-native/couchbase-lite';
-import { Http } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
+
+
+/**
+ * @name Couchbase Lite
+ * @description
+ * Plugin to install Couchbase Lite in your PhoneGap app on iOS or Android
+ *
+ * @usage
+ * ```typescript
+ * import { CouchbaseLite } from '@ionic-native/couchbase-lite';
+ * import { Http } from '@angular/http';
+ * import { Observable } from 'rxjs/Observable'
+ * constructor(private couchbase: CouchbaseLite, private platform:Platform,private _http:Http) {
+ *    this.initMethod();
+ * }
+ * url:string;
+ * initMethod() {
+ *    this.couchbase.getURL().then((url)=> {
+ *        this.url = url;
+ *    })
+ * }
+ * getUrl() {
+ *      return this.url;
+ * }
+ * createDatabase(database_name:string) {
+ *      let url = this.getUrl();
+ *      url = url+database_name;
+ *      return this._http
+ *        .put(url)
+ *        .map(data => { this.results = data['results'] })
+ *        .catch((error:any) => {
+ *           return Observable.throw(error.json() || 'Couchbase Lite error');
+ *         })
+ *  }
+ * 
+ * deleteDatabase(database_name:string) {
+ *      let url = this.getUrl();
+ *      url = url+database_name;
+ *      return this._http
+ *        .delete(url)
+ *        .map(data => { this.results = data['results'] })
+ *        .catch((error:any) => {
+ *           return Observable.throw(error.json() || 'Couchbase Lite error');
+ *        })
+ * }
+ *
+ * getAllDbs() {
+ *      let url = this.getUrl();
+ *      url = url+'_all_dbs';
+ *      return this._http
+ *        .get(url)
+ *        .map(data => { this.results = data['results'] })
+ *        .catch((error:any) => {
+ *           return Observable.throw(error.json() || 'Couchbase Lite error');
+ *        })
+ * }
+ *
+ *
+ *
+ *
+ * ```
+ */
+@Plugin({
+  pluginName: 'CouchbaseLite',
+  plugin: 'couchbase-lite-phonegap-plugin',
+  pluginRef: 'cblite',
+  repo: 'https://github.com/couchbaselabs/Couchbase-Lite-PhoneGap-Plugin',
+  platforms: ['Android', 'iOS']
+})
 @Injectable()
-export class LocalDatabaseService {
-  url:string;
-  constructor(
-        private couchbase: CouchbaseLite,
-        private platform:Platform,
-        private _http:Http) {
-            this.initMethode();
-  }
-  initMethode() {
-    this.couchbase.getURL().then((url)=> {
-        this.url = url;
-    })
-  }
-  getUrl() {
-    return this.url;
-  }
-  /* DATABASE RELATED METHODS */
-    /* create database */
-  createDatabase(database_name:string) {
-    let url = this.getUrl();
-    url = url+database_name;
-    return this._http
-        .put(url)
-        .map(data => { this.results = data['results'] })
-        .catch((error:any) => {
-            return Observable.throw(error.json() || 'Couchbase Lite error');
-        })
-  }
-    /* delete database */
-  deleteDatabase(database_name:string) {
-    let url = this.getUrl();
-    url = url+database_name;
-    return this._http
-        .delete(url)
-        .map(data => { this.results = data['results'] })
-        .catch((error:any) => {
-            return Observable.throw(error.json() || 'Couchbase Lite error');
-        })
-  }
-    /* get all database names */
-  getAllDbs() {
-    let url = this.getUrl();
-    url = url+'_all_dbs';
-    return this._http
-        .get(url)
-        .map(data => { this.results = data['results'] })
-        .catch((error:any) => {
-            return Observable.throw(error.json() || 'Couchbase Lite error');
-        })
-  }
+export class CouchbaseLite extends IonicNativePlugin {
+
+  /**
+   * Get the database url
+   * @return {Promise<any>} Returns a promise that resolves with the local database url
+   */
+  @Cordova({
+    callbackStyle: 'node'
+  })
+  getURL(): Promise<any> { return; }
+
 }
