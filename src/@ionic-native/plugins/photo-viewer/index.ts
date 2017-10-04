24d76ef6f8c4ae18dc5553818c37239cfa474a66
quentin <quentin.ligier@bluewin.ch>
2017-10-04 17:30:06 +0200

    docs(photo-viewer): improve options documentation

--- src/@ionic-native/plugins/photo-viewer/index.ts
+++ src/@ionic-native/plugins/photo-viewer/index.ts
@@ -1,5 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Plugin, Cordova, IonicNativePlugin } from '@ionic-native/core';
+
+export interface PhotoViewerOptions {
+  /**
+   * Set to false to disable the share button (Android only). Default: true
+   */
+  share?: boolean;
+}
+
 /**
  * @name Photo Viewer
  * @description This plugin can display your image in full screen with the ability to pan, zoom, and share the image.
@@ -29,8 +37,8 @@ export class PhotoViewer extends IonicNativePlugin {
    * Shows an image in full screen
    * @param url {string} URL or path to image
    * @param title {string}
-   * @param options {any}
+   * @param options {PhotoViewerOptions}
    */
   @Cordova({sync: true})
-  show(url: string, title?: string, options?: {share?: boolean}): void { }
+  show(url: string, title?: string, options?: PhotoViewerOptions): void { }
 }
