059ca0a3dac39c4b24cd761099730ca29d4c886e
Precious Jahlom Agboado <jahlom@preterk.com>
2017-11-02 07:40:30 +0000

    first implementation

--- src/@ionic-native/plugins/country/index.ts
+++ src/@ionic-native/plugins/country/index.ts
@@ -1,9 +1,9 @@
-import { Plugin, IonicNativePlugin } from '@ionic-native/core';
+import { Plugin, Cordova, IonicNativePlugin } from '@ionic-native/core';
 import { Injectable } from '@angular/core';
 
 /**
  * @name Country
- * @description
+ * @description Get the 2-letter ISO code from Android's Locale package. Useful when your device may not have GPS or cellular data.
  *
  * @usage
  * ```typescript
@@ -14,17 +14,26 @@ import { Injectable } from '@angular/core';
  *
  * ...
  *
- *
+ *  this.country.get()
+ *   .then(
+ *     isoCode => console.log('Locale is: ' + isoCode),
+ *     error => console.error('Error getting isoCode', error)
+ *   );
  * ```
  */
 @Plugin({
   pluginName: 'Country',
-  plugin: '',
-  pluginRef: '',
-  repo: '',
-  platforms: []
+  plugin: 'cordova-plugin-country',
+  pluginRef: 'plugins.country',
+  repo: 'https://github.com/cloakedninjas/cordova-plugin-country',
+  platforms: ["Android"]
 })
 @Injectable()
 export class Country extends IonicNativePlugin {
-
+  /**
+   * Get the 2-letter ISO code from Android's Locale package.
+   * @returns {Promise<string>} Returns a promise that resolves with the new image path, or rejects if failed to crop.
+   */
+  @Cordova()
+  get(): Promise<string> { return; }
 }
