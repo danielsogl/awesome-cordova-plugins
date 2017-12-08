997c96b97fb659485e9a8ca471c59d60b4943f12
Josh Babb <joshua@babb.host>
2017-12-08 13:50:20 -0600

    docs(android-permissions): note checkPermission return/result format and Android API version 26 changes that impact requestPermission and requestPermissions (#2183)
    * docs(android-permissions): note checkPermission() return/result format
    
    * docs(android-permissions): note requestPermission API ver. 26 behavior

--- src/@ionic-native/plugins/android-permissions/index.ts
+++ src/@ionic-native/plugins/android-permissions/index.ts
@@ -18,13 +18,15 @@ import { Injectable } from '@angular/core';
  * ...
  *
  * this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.CAMERA).then(
- *   success => console.log('Permission granted'),
+ *   result => console.log('Has permission?',result.hasPermission),
  *   err => this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.CAMERA)
  * );
  *
  * this.androidPermissions.requestPermissions([this.androidPermissions.PERMISSION.CAMERA, this.androidPermissions.PERMISSION.GET_ACCOUNTS]);
  *
  * ```
+ *
+ * Android 26 and above: due to Android 26's changes to permissions handling (permissions are requested at time of use rather than at runtime,) if your app does not include any functions (eg. other Ionic Native plugins) that utilize a particular permission, then `requestPermission()` and `requestPermissions()` will resolve immediately with no prompt shown to the user.  Thus, you must include a function utilizing the feature you would like to use before requesting permission for it.
  */
 @Plugin({
   pluginName: 'AndroidPermissions',
