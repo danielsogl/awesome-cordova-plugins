6d07cf1a844f1f5813fd52d3df26f2c9326e4942
Daniel Sogl <daniel@sogls.de>
2018-03-15 18:05:09 +0100

    fix lint

--- src/@ionic-native/plugins/web-intent/index.ts
+++ src/@ionic-native/plugins/web-intent/index.ts
@@ -35,77 +35,65 @@ import { Observable } from 'rxjs/Observable';
 })
 @Injectable()
 export class WebIntent extends IonicNativePlugin {
+  /**
+   * Convenience constant for actions
+   * @type {string}
+   */
+  @CordovaProperty ACTION_SEND: string;
 
   /**
    * Convenience constant for actions
    * @type {string}
    */
-  @CordovaProperty
-  ACTION_SEND: string;
-
-  /**
-   * Convenience constant for actions
-   * @type {string}
-   */
-  @CordovaProperty
-  ACTION_VIEW: string;
+  @CordovaProperty ACTION_VIEW: string;
 
   /**
    * Convenience constant for extras
    * @type {string}
    */
-  @CordovaProperty
-  EXTRA_TEXT: string;
+  @CordovaProperty EXTRA_TEXT: string;
 
   /**
    * Convenience constant for extras
    * @type {string}
    */
-  @CordovaProperty
-  EXTRA_SUBJECT: string;
+  @CordovaProperty EXTRA_SUBJECT: string;
 
   /**
    * Convenience constant for extras
    * @type {string}
    */
-  @CordovaProperty
-  EXTRA_STREAM: string;
+  @CordovaProperty EXTRA_STREAM: string;
 
   /**
    * Convenience constant for extras
    * @type {string}
    */
-  @CordovaProperty
-  EXTRA_EMAIL: string;
+  @CordovaProperty EXTRA_EMAIL: string;
 
   /**
    * Convenience constant for actions
    * @type {string}
    */
-  @CordovaProperty
-  ACTION_CALL: string;
+  @CordovaProperty ACTION_CALL: string;
 
   /**
    * Convenience constant for actions
    * @type {string}
    */
-  @CordovaProperty
-  ACTION_SENDTO: string;
+  @CordovaProperty ACTION_SENDTO: string;
 
   /**
    * Convenience constant for actions
    * @type {string}
    */
-  @CordovaProperty
-  ACTION_GET_CONTENT: string;
+  @CordovaProperty ACTION_GET_CONTENT: string;
 
   /**
    * Convenience constant for actions
    * @type {string}
    */
-  @CordovaProperty
-  ACTION_PICK: string;
-
+  @CordovaProperty ACTION_PICK: string;
 
   /**
    * Launches an Android intent
@@ -113,7 +101,13 @@ export class WebIntent extends IonicNativePlugin {
    * @returns {Promise<any>}
    */
   @Cordova()
-  startActivity(options: { action: any, url: string, type?: string }): Promise<any> { return; }
+  startActivity(options: {
+    action: any;
+    url: string;
+    type?: string;
+  }): Promise<any> {
+    return;
+  }
 
   /**
    * Starts a new activity and return the result to the application
@@ -121,7 +115,13 @@ export class WebIntent extends IonicNativePlugin {
    * @returns {Promise<any>}
    */
   @Cordova()
-  startActivityForResult(options: { action: any, url: string, type?: string }): Promise<any> { return; }
+  startActivityForResult(options: {
+    action: any;
+    url: string;
+    type?: string;
+  }): Promise<any> {
+    return;
+  }
 
   /**
    * Checks if this app was invoked with specified extra
@@ -129,7 +129,9 @@ export class WebIntent extends IonicNativePlugin {
    * @returns {Promise<any>}
    */
   @Cordova()
-  hasExtra(extra: string): Promise<any> { return; }
+  hasExtra(extra: string): Promise<any> {
+    return;
+  }
 
   /**
    * Gets the extra that this app was invoked with
@@ -137,14 +139,18 @@ export class WebIntent extends IonicNativePlugin {
    * @returns {Promise<any>}
    */
   @Cordova()
-  getExtra(extra: string): Promise<any> { return; }
+  getExtra(extra: string): Promise<any> {
+    return;
+  }
 
   /**
    * Gets the Uri the app was invoked with
    * @returns {Promise<any>}
    */
   @Cordova()
-  getUri(): Promise<string> { return; };
+  getUri(): Promise<string> {
+    return;
+  }
 
   /**
    * @returns {Observable<string>}
@@ -152,7 +158,9 @@ export class WebIntent extends IonicNativePlugin {
   @Cordova({
     observable: true
   })
-  onNewIntent(): Observable<string> { return; };
+  onNewIntent(): Observable<string> {
+    return;
+  }
 
   /**
    * Sends a custom intent passing optional extras
@@ -160,45 +168,56 @@ export class WebIntent extends IonicNativePlugin {
    * @returns {Promise<any>}
    */
   @Cordova()
-  sendBroadcast(options: { action: string, extras?: { option: boolean } }): Promise<any> { return; }
-  
-   /**
+  sendBroadcast(options: {
+    action: string;
+    extras?: { option: boolean };
+  }): Promise<any> {
+    return;
+  }
+
+  /**
    * Request that a given application service be started
    * @param options {Object} { action: string, extras?: { option: boolean } }
    * @returns {Promise<any>}
    */
   @Cordova()
-  startService(options: { action: string, extras?: { option: boolean } }): Promise<any> { return; }
+  startService(options: {
+    action: string;
+    extras?: { option: boolean };
+  }): Promise<any> {
+    return;
+  }
 
   /**
    * Registers a broadcast receiver for the specified filters
    * @param filters {any}
    */
   @Cordova({ sync: true })
-  registerBroadcastReceiver(filters: any): void { }
+  registerBroadcastReceiver(filters: any): void {}
 
   /**
    * Unregisters a broadcast receiver
    */
   @Cordova({ sync: true })
-  unregisterBroadcastReceiver(): void { }
+  unregisterBroadcastReceiver(): void {}
 
   /**
-  * Returns the content of the intent used whenever the application activity is launched
-  */
+   * Returns the content of the intent used whenever the application activity is launched
+   */
   @Cordova({ sync: true })
-  onIntent(): void { }
+  onIntent(): void {}
 
   /**
-  *
-  */
+   *
+   */
   @Cordova({ sync: true })
-  onActivityResult(): void { }
+  onActivityResult(): void {}
 
   /**
    * @returns {Promise<any>}
    */
   @Cordova()
-  getIntent(): Promise<any> { return; };
-
+  getIntent(): Promise<any> {
+    return;
+  }
 }
