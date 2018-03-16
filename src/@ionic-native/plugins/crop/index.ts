504838556ea79061cd02534925292fff9e4b548f
Daniel Sogl <daniel@sogls.de>
2018-03-16 09:01:33 +0100

    Update index.ts

--- src/@ionic-native/plugins/crop/index.ts
+++ src/@ionic-native/plugins/crop/index.ts
@@ -2,9 +2,9 @@ import { Injectable } from '@angular/core';
 import { Cordova, Plugin, IonicNativePlugin } from '@ionic-native/core';
 
 export interface CropOptions {
-  quality: number;
-  targetHeight: number;
-  targetWidth: number;
+  quality?: number;
+  targetHeight?: number;
+  targetWidth?: number;
 }
 
 /**
