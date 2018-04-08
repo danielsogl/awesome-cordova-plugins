d598bf34169c92e2de7382c492a4c1f3999c9d4e
Daniel <mytechde@outlook.com>
2018-04-08 21:30:20 +0200

    docs(file-encryption): typo

--- src/@ionic-native/plugins/file-encryption/index.ts
+++ src/@ionic-native/plugins/file-encryption/index.ts
@@ -1,5 +1,5 @@
-import { Plugin, Cordova, IonicNativePlugin } from '@ionic-native/core';
 import { Injectable } from '@angular/core';
+import { Cordova, IonicNativePlugin, Plugin } from '@ionic-native/core';
 
 /**
  * @name File Encryption
@@ -30,23 +30,25 @@ import { Injectable } from '@angular/core';
 })
 @Injectable()
 export class FileEncryption extends IonicNativePlugin {
-
   /**
-   * Enrcypt a file
-   * @param file {string} A string representing a local URI
-   * @param key {string} A key for the crypto operations
+   * Encrypt a file
+   * @param {string} file A string representing a local URI
+   * @param {string} key A key for the crypto operations
    * @return {Promise<any>} Returns a promise that resolves when something happens
    */
   @Cordova()
-  encrypt(file: string, key: string): Promise<any> { return; }
+  encrypt(file: string, key: string): Promise<any> {
+    return;
+  }
 
   /**
    * Decrypt a file
-   * @param file {string} A string representing a local URI
-   * @param key {string} A key for the crypto operations
+   * @param {string} file A string representing a local URI
+   * @param {string} key A key for the crypto operations
    * @return {Promise<any>} Returns a promise that resolves when something happens
    */
   @Cordova()
-  decrypt(file: string, key: string): Promise<any> { return; }
-
+  decrypt(file: string, key: string): Promise<any> {
+    return;
+  }
 }
