d4c98362d93fa7051bca9d6b62b3842bfe4c4d13
Daniel Sogl <daniel@sogls.de>
2018-04-15 10:18:40 +0200

    docs(firebase): remove beta notice

--- src/@ionic-native/plugins/firebase/index.ts
+++ src/@ionic-native/plugins/firebase/index.ts
@@ -3,7 +3,6 @@ import { Cordova, IonicNativePlugin, Plugin } from '@ionic-native/core';
 import { Observable } from 'rxjs/Observable';
 
 /**
- * @beta
  * @name Firebase
  * @description
  * This plugin brings push notifications, analytics, event tracking, crash reporting and more from Google Firebase to your Cordova project! Android and iOS supported (including iOS 10).
