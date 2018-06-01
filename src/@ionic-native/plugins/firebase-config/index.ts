9e7121cf55b215c9bfd29fa5583766283a6b75bb
Adam Duren <adamduren@gmail.com>
2018-06-01 18:12:55 -0400

    feat(firebase-config): add firebase-config wrapper

---
+++ src/@ionic-native/plugins/firebase-config/index.ts
@@ -0,0 +1,91 @@
+import { Injectable } from '@angular/core';
+import { Plugin, Cordova, IonicNativePlugin } from '@ionic-native/core';
+
+/**
+ * @beta
+ * @name Firebase Config
+ * @description
+ * Cordova plugin for Firebase Config
+ *
+ * @usage
+ * ```typescript
+ * import { FirebaseConfig } from '@ionic-native/firebase-config';
+ *
+ *
+ * constructor(private firebaseConfig: FirebaseConfig) { }
+ *
+ * ...
+ *
+ *
+ * this.firebaseConfig.getBoolean('my_key')
+ *   .then((res: any) => console.log(res))
+ *   .catch((error: any) => console.error(error));
+ *
+ * ```
+ */
+@Plugin({
+  pluginName: 'FirebaseConfig',
+  plugin: 'cordova-plugin-firebase-config',
+  pluginRef: 'cordova.plugins.firebase.config',
+  repo: 'https://github.com/chemerisuk/cordova-plugin-firebase-config',
+  platforms: ['Android', 'iOS']
+})
+@Injectable()
+export class FirebaseConfig extends IonicNativePlugin {
+
+  /**
+   * Fetches remote config values with appropriate TTL and then activates them.
+   *
+   * @param {number} ttlSeconds
+   * @returns {Promise<null>}
+   */
+  @Cordova({ sync: true })
+  update(ttlSeconds: number, namespace?: string): Promise<null> {
+    return;
+  }
+
+  /**
+   * Fetches a boolean configuration value from RemoteConfig
+   *
+   * @param {string} key
+   * @returns {Promise<boolean>}
+   */
+  @Cordova({ sync: true })
+  getBoolean(key: string, namespace?: string): Promise<boolean> {
+    return;
+  }
+
+  /**
+   * Fetches a string configuration value from RemoteConfig
+   *
+   * @param {string} key
+   * @returns {Promise<boolean>}
+   */
+  @Cordova({ sync: true })
+  getString(key: string, namespace?: string): Promise<string> {
+    return;
+  }
+
+  /**
+   * Fetches a numeric configuration value from RemoteConfig
+   *
+   * @param {string} key
+   * @returns {Promise<boolean>}
+   */
+  @Cordova({ sync: true })
+  getNumber(key: string, namespace?: string): Promise<number> {
+    return;
+  }
+
+  /**
+   * Fetches an array of bytes configuration value from RemoteConfig
+   *
+   * @param {string} key
+   * @returns {Promise<boolean>}
+   */
+  @Cordova({ sync: true })
+  getBytes(key: string, namespace?: string): Promise<any[]> {
+    return;
+  }
+
+}
