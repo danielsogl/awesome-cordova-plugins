abb64ab0034053eddb3dd01f260859765e64ec8e
GabrielTK <enricorocha1@hotmail.com>
2018-06-06 14:24:04 -0300

    refactor(hce): refactor to comply with guidelines

--- src/@ionic-native/plugins/hce/index.ts
+++ src/@ionic-native/plugins/hce/index.ts
@@ -1,18 +1,6 @@
-/**
- * This is a template for new plugin wrappers
- *
- * TODO:
- * - Add/Change information below
- * - Document usage (importing, executing main functionality)
- * - Remove any imports that you are not using
- * - Remove all the comments included in this template, EXCEPT the @Plugin wrapper docs and any other docs you added
- * - Remove this note
- *
- */
+
 import { Injectable } from '@angular/core';
 import { Plugin, Cordova, IonicNativePlugin } from '@ionic-native/core';
-// import { Observable } from 'rxjs/Observable';
-// import {CordovaProperty, CordovaInstance, InstanceProperty} from '@ionic-native/core'
 
 /**
  * @name hce
@@ -43,12 +31,12 @@ import { Plugin, Cordova, IonicNativePlugin } from '@ionic-native/core';
  */
 @Plugin({
   pluginName: 'hce',
-  plugin: 'cordova-plugin-hce', // npm package name, example: cordova-plugin-camera
-  pluginRef: 'hce', // the variable reference to call the plugin, example: navigator.geolocation
-  repo: 'https://github.com/don/cordova-plugin-hce', // the github repository URL for the plugin
-  install: '', // OPTIONAL install command, in case the plugin requires variables
-  installVariables: ['AID_FILTER'], // OPTIONAL the plugin requires variables
-  platforms: ['Android'] // Array of platforms supported, example: ['Android', 'iOS']
+  plugin: 'cordova-plugin-hce',
+  pluginRef: 'hce',
+  repo: 'https://github.com/don/cordova-plugin-hce',
+  install: '',
+  installVariables: ['AID_FILTER'],
+  platforms: ['Android']
 })
 @Injectable()
 export class HCE extends IonicNativePlugin {
@@ -86,34 +74,7 @@ export class HCE extends IonicNativePlugin {
   sendResponse(response: Uint8Array, success?: Function, failure?: Function): void {
     return; // We add return; here to avoid any IDE / Compiler errors
   }
-
-
 }
-/*
-class Util{
-  /**
-   * Converts Hex to Byte array.
-   * @param hex {string} HEX String
-   * @returns {Uint8Array}
-   * 
-   *//*
-  @Cordova()
-  hexStringToByteArray(hex :string): Uint8Array {
-    return; // We add return; here to avoid any IDE / Compiler errors
-  }
-  /**
-   * Converts Byte array to Hex String.
-   * @param byte {Uint8Array} Byte Array
-   * @returns {string}
-   * 
-   *//*
-  @Cordova()
-  byteArrayToHexString(array :Uint8Array): string {
-    return; // We add return; here to avoid any IDE / Compiler errors
-  }
-  
-}*/
 
 export interface HCECommandEvent { (command: Uint8Array): void; }
 export interface HCEDeactivatedEvent { (command: number): void; }
-
