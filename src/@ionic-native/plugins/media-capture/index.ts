5803c069a3d2c034aacebfa3b5c70e343dff64d4
Cristian Rivas Gómez <rivasgomezcristian@gmail.com>
2018-06-07 08:47:38 +0200

    Add Media Capture plugin description to documentation (#2539)

--- src/@ionic-native/plugins/media-capture/index.ts
+++ src/@ionic-native/plugins/media-capture/index.ts
@@ -115,6 +115,10 @@ export interface ConfigurationData {
 /**
  * @name Media Capture
  * @description
+ * This plugin provides access to the device's audio, image, and video capture capabilities.
+ *
+ * Requires Cordova plugin: `cordova-plugin-media-capture`. For more info, please see the [Media Capture plugin docs](https://github.com/apache/cordova-plugin-media-capture).
+ *
  * @usage
  * ```typescript
  * import { MediaCapture, MediaFile, CaptureError, CaptureImageOptions } from '@ionic-native/media-capture';
