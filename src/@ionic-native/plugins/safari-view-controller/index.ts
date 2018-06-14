dd769b0226421d00cb74989f00e3f622b0ba47a0
Cristian Rivas Gómez <rivasgomezcristian@gmail.com>
2018-06-14 07:58:38 +0200

    docs(safari-view-controller): add plugin description (#2552)

--- src/@ionic-native/plugins/safari-view-controller/index.ts
+++ src/@ionic-native/plugins/safari-view-controller/index.ts
@@ -18,6 +18,10 @@ export interface SafariViewControllerOptions {
 /**
  * @name Safari View Controller
  * @description
+ * For displaying read-only web content.
+ *
+ * Requires Cordova plugin: `cordova-plugin-safariviewcontroller`. For more info, please see the [Safari View Controller plugin docs](https://github.com/EddyVerbruggen/cordova-plugin-safariviewcontroller).
+ *
  * @usage
  * ```typescript
  * import { SafariViewController } from '@ionic-native/safari-view-controller';
