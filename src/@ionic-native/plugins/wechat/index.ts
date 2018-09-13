69b1ceafc3f68788774bbc2df5928996a3b07624
alex <dong,xingbin,alex@gmail.com>
2018-09-13 10:33:22 +0800

    add new plugin of wechat

---
+++ src/@ionic-native/plugins/wechat/index.ts
@@ -0,0 +1,178 @@
+/**
+ * This is a template for new plugin wrappers
+ *
+ * TODO:
+ * - Add/Change information below
+ * - Document usage (importing, executing main functionality)
+ * - Remove any imports that you are not using
+ * - Remove all the comments included in this template, EXCEPT the @Plugin wrapper docs and any other docs you added
+ * - Remove this note
+ *
+ */
+import { Injectable } from '@angular/core';
+import { Plugin, Cordova, IonicNativePlugin } from '@ionic-native/core';
+
+/**
+ * @name Wechat
+ * @description
+ * This plugin does something
+ *
+ * @usage
+ * ```typescript
+ * import { Wechat } from '@ionic-native/wechat';
+ *
+ *
+ * constructor(private wechat: Wechat) { }
+ *
+ * ...
+ *
+ *
+ * this.wechat.functionName('Hello', 123)
+ *   .then((res: any) => console.log(res))
+ *   .catch((error: any) => console.error(error));
+ *
+ * ```
+ */
+@Plugin({
+  pluginName: 'Wechat',
+  plugin: 'cordova-plugin-wechat', // npm package name, example: cordova-plugin-camera
+  pluginRef: 'Wechat', // the variable reference to call the plugin, example: navigator.geolocation
+  repo: 'https://github.com/xu-li/cordova-plugin-wechat.git', // the github repository URL for the plugin
+  install: 'cordova plugin add cordova-plugin-wechat --variable wechatappid=YOUR_WECHAT_APPID', // OPTIONAL install command, in case the plugin requires variables
+  installVariables: ['wechatappid'], // OPTIONAL the plugin requires variables
+  platforms: ['Android', 'iOS'] // Array of platforms supported, example: ['Android', 'iOS']
+})
+@Injectable()
+export class Wechat extends IonicNativePlugin {
+
+  Scene: {
+    SESSION: 0, // 聊天界面
+    TIMELINE: 1, // 朋友圈
+    FAVORITE: 2  // 收藏
+  };
+
+  Type: {
+    APP: 1,
+    EMOTION: 2,
+    FILE: 3,
+    IMAGE: 4,
+    MUSIC: 5,
+    VIDEO: 6,
+    WEBPAGE: 7
+  };
+
+  @Cordova()
+  isInstalled(): Promise<any> {
+    return; // We add return; here to avoid any IDE / Compiler errors
+  }
+
+  /**
+   * Share a message to wechat app
+   *
+   * @example
+   * <code>
+   * this.wechat.share({
+     *     message: {
+     *        title: "Message Title",
+     *        description: "Message Description(optional)",
+     *        mediaTagName: "Media Tag Name(optional)",
+     *        thumb: "http://YOUR_THUMBNAIL_IMAGE",
+     *        media: {
+     *            type: Wechat.Type.WEBPAGE,   // webpage
+     *            webpageUrl: "https://github.com/xu-li/cordova-plugin-wechat"    // webpage
+     *        }
+     *    },
+     *    scene: this.wechat.Scene.TIMELINE   // share to Timeline
+     * }.then(() => {
+     *       console.log('share success');
+     *    })
+     *    .catch(error => {
+     *       console.log(error);
+     *    });
+     * </code>
+   */
+  @Cordova()
+  share(params: any): Promise<any> {
+    return; // We add return; here to avoid any IDE / Compiler errors
+  }
+
+  /**
+   * Sending an auth request to Wechat
+   *
+   * @example
+   * <code>
+   * this.wechat.auth.then((res) => { alert(res.code); });
+   * </code>
+   */
+  auth(scope: any, state: any): Promise<any> {
+    return; // We add return; here to avoid any IDE / Compiler errors
+  }
+
+  /**
+   * Send a payment request
+   *
+   * @link https://pay.weixin.qq.com/wiki/doc/api/app.php?chapter=9_1
+   * @example
+   * <code>
+   * var params = {
+     *     mch_id: '10000100', // merchant id
+     *     prepay_id: 'wx201411101639507cbf6ffd8b0779950874', // prepay id returned from server
+     *     nonce: '1add1a30ac87aa2db72f57a2375d8fec', // nonce string returned from server
+     *     timestamp: '1439531364', // timestamp
+     *     sign: '0CB01533B8C1EF103065174F50BCA001', // signed string
+     * };
+   * this.wechat.sendPaymentRequest(params).then(() => {
+     *     console.log("Success");
+     * }).catch(error => {
+     *     console.log(error);
+     * });
+   * </code>
+   */
+  sendPaymentRequest(params: any): Promise<any> {
+    return; // We add return; here to avoid any IDE / Compiler errors
+  }
+
+  /**
+   * jumpToBizProfile （跳转到某个微信公众号）2016-11-11 测试是失效的，囧
+   *
+   * @link https://segmentfault.com/a/1190000007204624
+   * @link https://segmentfault.com/q/1010000003907796
+   * @example
+   * <code>
+   * var params = {
+     *     info: 'gh_xxxxxxx', // 公众帐号原始ID
+     *     type:  'Normal' // 普通号
+     * }
+   * or
+   * var params = {
+     *     info: 'extMsg', // 相关的硬件二维码串
+     *     type:  'Device' // 硬件号
+     * };
+   * this.wechat.jumpToBizProfile(params).then(()=> {
+     *     console.log("Success");
+     * }).catch(error => {
+     *     console.log(error);
+     * });
+   * </code>
+   */
+  jumpToWechat(url: string): Promise<any> {
+    return; // We add return; here to avoid any IDE / Compiler errors
+  }
+
+  /**
+   * chooseInvoiceFromWX exq:choose invoices from Wechat card list
+   *
+   * @example
+   * <code>
+   * params: signType, cardSign, nonceStr, timeStamp  all required
+   * this.wechat.chooseInvoiceFromWX(params).then(() => {
+     *     console.log("Success");
+     * }).catch(error => {
+     *     console.log(error);
+     * });
+   * </code>
+   */
+  chooseInvoiceFromWX(params: any): Promise<any> {
+    return; // We add return; here to avoid any IDE / Compiler errors
+  }
+}
