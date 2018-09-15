f8f4db8f0e2e304ad387514b8cd1606fb8e841eb
Daniel Sogl <daniel@sogls.de>
2018-09-15 10:57:51 +0200

    Update index.ts

--- src/@ionic-native/plugins/wechat/index.ts
+++ src/@ionic-native/plugins/wechat/index.ts
@@ -1,16 +1,5 @@
-/**
- * This is a template for new plugin wrappers
- *
- * TODO:
- * - Add/Change information below
- * - Document usage (importing, executing main functionality)
- * - Remove any imports that you are not using
- * - Remove all the comments included in this template, EXCEPT the @Plugin wrapper docs and any other docs you added
- * - Remove this note
- *
- */
 import { Injectable } from '@angular/core';
-import { Plugin, Cordova, IonicNativePlugin } from '@ionic-native/core';
+import { Cordova, IonicNativePlugin, Plugin } from '@ionic-native/core';
 
 /**
  * @name Wechat
@@ -35,30 +24,29 @@ import { Plugin, Cordova, IonicNativePlugin } from '@ionic-native/core';
  */
 @Plugin({
   pluginName: 'Wechat',
-  plugin: 'cordova-plugin-wechat', // npm package name, example: cordova-plugin-camera
-  pluginRef: 'Wechat', // the variable reference to call the plugin, example: navigator.geolocation
-  repo: 'https://github.com/xu-li/cordova-plugin-wechat.git', // the github repository URL for the plugin
-  install: 'cordova plugin add cordova-plugin-wechat --variable wechatappid=YOUR_WECHAT_APPID', // OPTIONAL install command, in case the plugin requires variables
-  installVariables: ['wechatappid'], // OPTIONAL the plugin requires variables
-  platforms: ['Android', 'iOS'] // Array of platforms supported, example: ['Android', 'iOS']
+  plugin: 'cordova-plugin-wechat',
+  pluginRef: 'Wechat',
+  repo: 'https://github.com/xu-li/cordova-plugin-wechat.git',
+  install: 'cordova plugin add cordova-plugin-wechat --variable wechatappid=YOUR_WECHAT_APPID',
+  installVariables: ['wechatappid'],
+  platforms: ['Android', 'iOS']
 })
 @Injectable()
 export class Wechat extends IonicNativePlugin {
-
   Scene: {
-    SESSION: 0, // 聊天界面
-    TIMELINE: 1, // 朋友圈
-    FAVORITE: 2  // 收藏
+    SESSION: 0; // 聊天界面
+    TIMELINE: 1; // 朋友圈
+    FAVORITE: 2; // 收藏
   };
 
   Type: {
-    APP: 1,
-    EMOTION: 2,
-    FILE: 3,
-    IMAGE: 4,
-    MUSIC: 5,
-    VIDEO: 6,
-    WEBPAGE: 7
+    APP: 1;
+    EMOTION: 2;
+    FILE: 3;
+    IMAGE: 4;
+    MUSIC: 5;
+    VIDEO: 6;
+    WEBPAGE: 7;
   };
 
   @Cordova()
@@ -72,24 +60,24 @@ export class Wechat extends IonicNativePlugin {
    * @example
    * <code>
    * this.wechat.share({
-     *     message: {
-     *        title: "Message Title",
-     *        description: "Message Description(optional)",
-     *        mediaTagName: "Media Tag Name(optional)",
-     *        thumb: "http://YOUR_THUMBNAIL_IMAGE",
-     *        media: {
-     *            type: Wechat.Type.WEBPAGE,   // webpage
-     *            webpageUrl: "https://github.com/xu-li/cordova-plugin-wechat"    // webpage
-     *        }
-     *    },
-     *    scene: this.wechat.Scene.TIMELINE   // share to Timeline
-     * }.then(() => {
-     *       console.log('share success');
-     *    })
-     *    .catch(error => {
-     *       console.log(error);
-     *    });
-     * </code>
+   *     message: {
+   *        title: "Message Title",
+   *        description: "Message Description(optional)",
+   *        mediaTagName: "Media Tag Name(optional)",
+   *        thumb: "http://YOUR_THUMBNAIL_IMAGE",
+   *        media: {
+   *            type: Wechat.Type.WEBPAGE,   // webpage
+   *            webpageUrl: "https://github.com/xu-li/cordova-plugin-wechat"    // webpage
+   *        }
+   *    },
+   *    scene: this.wechat.Scene.TIMELINE   // share to Timeline
+   * }.then(() => {
+   *       console.log('share success');
+   *    })
+   *    .catch(error => {
+   *       console.log(error);
+   *    });
+   * </code>
    */
   @Cordova()
   share(params: any): Promise<any> {
@@ -115,17 +103,17 @@ export class Wechat extends IonicNativePlugin {
    * @example
    * <code>
    * var params = {
-     *     mch_id: '10000100', // merchant id
-     *     prepay_id: 'wx201411101639507cbf6ffd8b0779950874', // prepay id returned from server
-     *     nonce: '1add1a30ac87aa2db72f57a2375d8fec', // nonce string returned from server
-     *     timestamp: '1439531364', // timestamp
-     *     sign: '0CB01533B8C1EF103065174F50BCA001', // signed string
-     * };
+   *     mch_id: '10000100', // merchant id
+   *     prepay_id: 'wx201411101639507cbf6ffd8b0779950874', // prepay id returned from server
+   *     nonce: '1add1a30ac87aa2db72f57a2375d8fec', // nonce string returned from server
+   *     timestamp: '1439531364', // timestamp
+   *     sign: '0CB01533B8C1EF103065174F50BCA001', // signed string
+   * };
    * this.wechat.sendPaymentRequest(params).then(() => {
-     *     console.log("Success");
-     * }).catch(error => {
-     *     console.log(error);
-     * });
+   *     console.log("Success");
+   * }).catch(error => {
+   *     console.log(error);
+   * });
    * </code>
    */
   sendPaymentRequest(params: any): Promise<any> {
@@ -140,19 +128,19 @@ export class Wechat extends IonicNativePlugin {
    * @example
    * <code>
    * var params = {
-     *     info: 'gh_xxxxxxx', // 公众帐号原始ID
-     *     type:  'Normal' // 普通号
-     * }
+   *     info: 'gh_xxxxxxx', // 公众帐号原始ID
+   *     type:  'Normal' // 普通号
+   * }
    * or
    * var params = {
-     *     info: 'extMsg', // 相关的硬件二维码串
-     *     type:  'Device' // 硬件号
-     * };
+   *     info: 'extMsg', // 相关的硬件二维码串
+   *     type:  'Device' // 硬件号
+   * };
    * this.wechat.jumpToBizProfile(params).then(()=> {
-     *     console.log("Success");
-     * }).catch(error => {
-     *     console.log(error);
-     * });
+   *     console.log("Success");
+   * }).catch(error => {
+   *     console.log(error);
+   * });
    * </code>
    */
   jumpToWechat(url: string): Promise<any> {
@@ -166,10 +154,10 @@ export class Wechat extends IonicNativePlugin {
    * <code>
    * params: signType, cardSign, nonceStr, timeStamp  all required
    * this.wechat.chooseInvoiceFromWX(params).then(() => {
-     *     console.log("Success");
-     * }).catch(error => {
-     *     console.log(error);
-     * });
+   *     console.log("Success");
+   * }).catch(error => {
+   *     console.log(error);
+   * });
    * </code>
    */
   chooseInvoiceFromWX(params: any): Promise<any> {
