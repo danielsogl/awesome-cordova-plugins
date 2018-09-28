fc0292d0df1b89a2124268419f391f47379cc18a
Faizan Hasan <sfaizanh@users.noreply.github.com>
2018-09-28 18:48:13 +0500

    fix(CordovaProperty) Imported CordovaProperty from core

--- src/@ionic-native/plugins/keyboard/index.ts
+++ src/@ionic-native/plugins/keyboard/index.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Cordova, IonicNativePlugin, Plugin } from '@ionic-native/core';
+import { Cordova, CordovaProperty, IonicNativePlugin, Plugin } from '@ionic-native/core';
 import { Observable } from 'rxjs/Observable';
 
 /**
