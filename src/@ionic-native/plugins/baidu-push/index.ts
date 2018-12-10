3ef41145087a273a71e8ad21735ac1e1d8425888
Eric Horodyski <e.horodyski@ieee.org>
2018-12-10 10:33:43 -0500

    Initial push.

---
+++ src/@ionic-native/plugins/baidu-push/index.ts
@@ -0,0 +1,60 @@
+/**
+ * This is a template for new plugin wrappers
+ *
+ * TODO:
+ * - Add/Change information below
+ * - Document usage (importing, executing main functionality)
+ * - Remove any imports that you are not using
+ * - Remove all the comments included in this template, EXCEPT the @Plugin wrapper docs and any other docs you added
+ * - Remove this note
+ *
+ */
+import { Injectable } from '@angular/core';
+import { Plugin, Cordova, CordovaProperty, CordovaInstance, InstanceProperty, IonicNativePlugin } from '@ionic-native/core';
+import { Observable } from 'rxjs/Observable';
+
+/**
+ * @name Baidu Push
+ * @description
+ * This plugin does something
+ *
+ * @usage
+ * ```typescript
+ * import { BaiduPush } from '@ionic-native/baidu-push';
+ *
+ *
+ * constructor(private baiduPush: BaiduPush) { }
+ *
+ * ...
+ *
+ *
+ * this.baiduPush.functionName('Hello', 123)
+ *   .then((res: any) => console.log(res))
+ *   .catch((error: any) => console.error(error));
+ *
+ * ```
+ */
+@Plugin({
+  pluginName: 'BaiduPush',
+  plugin: '', // npm package name, example: cordova-plugin-camera
+  pluginRef: '', // the variable reference to call the plugin, example: navigator.geolocation
+  repo: '', // the github repository URL for the plugin
+  install: '', // OPTIONAL install command, in case the plugin requires variables
+  installVariables: [], // OPTIONAL the plugin requires variables
+  platforms: [] // Array of platforms supported, example: ['Android', 'iOS']
+})
+@Injectable()
+export class BaiduPush extends IonicNativePlugin {
+
+  /**
+   * This function does something
+   * @param arg1 {string} Some param to configure something
+   * @param arg2 {number} Another param to configure something
+   * @return {Promise<any>} Returns a promise that resolves when something happens
+   */
+  @Cordova()
+  functionName(arg1: string, arg2: number): Promise<any> {
+    return; // We add return; here to avoid any IDE / Compiler errors
+  }
+
+}
