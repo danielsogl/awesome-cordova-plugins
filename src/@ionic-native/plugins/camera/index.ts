f231c0cd2e30109c2ebd9cd448167807c4caec68
Daniel Sogl <daniel@sogls.de>
2018-12-27 09:28:05 +0100

    Update index.ts

--- src/@ionic-native/plugins/camera/index.ts
+++ src/@ionic-native/plugins/camera/index.ts
@@ -128,9 +128,9 @@ export enum Direction {
  *
  * Requires the Cordova plugin: `cordova-plugin-camera`. For more info, please see the [Cordova Camera Plugin Docs](https://github.com/apache/cordova-plugin-camera).
  *
- * [Warning] Since IOS 10 the camera requires permissions to be placed in your config.xml add 
+ * [Warning] Since IOS 10 the camera requires permissions to be placed in your config.xml add
  * ```xml
- *<config-file parent="NSCameraUsageDescription" platform="ios" target="*-Info.plist">
+ * <config-file parent="NSCameraUsageDescription" platform="ios" target="*-Info.plist">
  *  <string>You can take photos</string>
  * </config-file>
  * ```
