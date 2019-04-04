16c200f09d6ba252e7f3d20b7b067ce7fb878b59
Julián Mesa Llopis <jmesa@sistel.es>
2019-04-04 16:28:48 +0200

    Fix disconnect return. Fix close return. Fix discover return. Fix services return. Fix mtu return.

--- src/@ionic-native/plugins/bluetooth-le/index.ts
+++ src/@ionic-native/plugins/bluetooth-le/index.ts
@@ -579,10 +579,10 @@ export class BluetoothLE extends IonicNativePlugin {
    * Disconnect from a Bluetooth LE device.
    *              Note: It's simpler to just call close(). Starting with iOS 10, disconnecting before closing seems required!
    * @param {{address: string}} params The address/identifier
-   * @returns {Promise<{ status: DeviceInfo }>}
+   * @returns {Promise<DeviceInfo>}
    */
   @Cordova({ callbackOrder: 'reverse' })
-  disconnect(params: { address: string }): Promise<{ status: DeviceInfo }> {
+  disconnect(params: { address: string }): Promise<DeviceInfo> {
     return;
   }
 
@@ -592,10 +592,10 @@ export class BluetoothLE extends IonicNativePlugin {
    * Prior to 2.7.0, you needed to disconnect to the device before closing, but this is no longer the case.
    * Starting with iOS 10, disconnecting before closing seems required!
    * @param {{ address: string }} params The address/identifier
-   * @returns {Promise<{ status: DeviceInfo }>}
+   * @returns {Promise<DeviceInfo>}
    */
   @Cordova({ callbackOrder: 'reverse' })
-  close(params: { address: string }): Promise<{ status: DeviceInfo }> {
+  close(params: { address: string }): Promise<DeviceInfo> {
     return;
   }
 
@@ -609,7 +609,7 @@ export class BluetoothLE extends IonicNativePlugin {
    * Subsequent discover events will make use of this cache.
    * If your device's services change, set the clearCache parameter to force Android to re-discover services.
    * @param {{ address: string, clearCache: boolean }} params The address/identifier
-   * @returns {Promise<{ device: Device }>}
+   * @returns {Promise<Device>}
    *    success: device object (contains array of service objects)
    *    error: The callback that will be triggered when the unbond operation fails
    */
@@ -617,7 +617,7 @@ export class BluetoothLE extends IonicNativePlugin {
   discover(params: {
     address: string;
     clearCache?: boolean;
-  }): Promise<{ device: Device }> {
+  }): Promise<Device> {
     return;
   }
 
@@ -626,13 +626,13 @@ export class BluetoothLE extends IonicNativePlugin {
    * Discover the device's services.
    * Not providing an array of services will return all services and take longer to discover. iOS support only.
    * @param {{address: string, services: string[]}} params
-   * @returns {Promise<{ services: Services }>}
+   * @returns {Promise<Services>}
    */
   @Cordova({ callbackOrder: 'reverse' })
   services(params: {
     address: string;
     services?: string[];
-  }): Promise<{ services: Services }> {
+  }): Promise<Services> {
     return;
   }
 
@@ -747,7 +747,7 @@ export class BluetoothLE extends IonicNativePlugin {
    * @name rssi
    * Read RSSI of a connected device. RSSI is also returned with scanning.
    * @param {{ address: string }} params
-   * @returns {Promise<{ rssi: RSSI }>}
+   * @returns {Promise< RSSI >}
    */
   @Cordova({ callbackOrder: 'reverse' })
   rssi(params: { address: string }): Promise< RSSI > {
@@ -758,10 +758,10 @@ export class BluetoothLE extends IonicNativePlugin {
    * @name mtu (Android, Android 5+)
    * Set MTU of a connected device. Android only.
    * @param {{ address: string, mtu: number }} params
-   * @returns {Promise<{ mtu: MTU }>}
+   * @returns {Promise< MTU >}
    */
   @Cordova({ callbackOrder: 'reverse' })
-  mtu(params: { address: string; mtu?: number }): Promise<{ mtu: MTU }> {
+  mtu(params: { address: string; mtu?: number }): Promise< MTU > {
     return;
   }
 
