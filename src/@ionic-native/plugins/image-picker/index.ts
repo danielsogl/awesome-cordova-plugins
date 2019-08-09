852df986476b133ab71c97042ef6863ce8a6002e
Matthew Harris <rtpharry@hotmail.com>
2019-08-09 16:05:53 +0700

    docs(image-picker): set repo url to active repo (#3127)
    Original repo hasn't had a commit since 2016. `Repo:` parameter already updated but intro docs wasn't.

--- src/@ionic-native/plugins/image-picker/index.ts
+++ src/@ionic-native/plugins/image-picker/index.ts
@@ -42,7 +42,7 @@ export enum OutputType {
  * Cordova Plugin For Multiple Image Selection
  *
  * Requires Cordova plugin: `cordova-plugin-image-picker`.
- * For more info, please see the https://github.com/wymsee/cordova-imagePicker
+ * For more info, please see the https://github.com/Telerik-Verified-Plugins/ImagePicker
  *
  * @usage
  * ```typescript
