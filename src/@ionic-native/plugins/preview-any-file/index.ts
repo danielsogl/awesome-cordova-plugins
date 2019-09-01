341f6ef43ab6e47a473bd2dc77a4c1df1db9ec3a
Mostafa Mansour <mostafa@outlook.kr>
2019-09-02 00:46:44 +0400

    add the reop link

--- src/@ionic-native/plugins/preview-any-file/index.ts
+++ src/@ionic-native/plugins/preview-any-file/index.ts
@@ -5,6 +5,8 @@ import { Plugin, Cordova, IonicNativePlugin } from '@ionic-native/core';
  * @description
  * Whatever the file is PDF document, Word document, Excel, office document,zip archive file, image, text, html or anything else, you can perform a preview by this cordova Plugin to preview any file in native mode by providing the local or external URL.
  *
+ *  Requires Cordova plugin: `cordova-plugin-preview-any-file`. For more info, please see the [previewAnyFile plugin docs](https://github.com/mostafa-mansour1/previewAnyFile).
+ *
  * @usage
  * ```typescript
  * import { PreviewAnyFile } from '@ionic-native/preview-any-file';
