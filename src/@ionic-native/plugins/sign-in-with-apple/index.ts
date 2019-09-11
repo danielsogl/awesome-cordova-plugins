45818f3e6ab6b7af4053ee0ccef5895e39352882
keiya <keiya_21@yahoo.co.jp>
2019-09-12 07:34:02 +0900

    docs(sign-in-with-apple): add comments

--- src/@ionic-native/plugins/sign-in-with-apple/index.ts
+++ src/@ionic-native/plugins/sign-in-with-apple/index.ts
@@ -2,6 +2,27 @@ import { Injectable } from '@angular/core';
 import { Plugin, Cordova, CordovaProperty, CordovaInstance, InstanceProperty, IonicNativePlugin } from '@ionic-native/core';
 import { Observable } from 'rxjs';
 
+/**
+ * @name SignInWithApple
+ * @description
+ * This plugin provides Sign in With Apple native feature
+ *
+ * @usage
+ * ```typescript
+ * import { SignInWithApple } from '@ionic-native/sign-in-with-apple';
+ *
+ *
+ * constructor(private signInWithApple: SignInWithApple) { }
+ *
+ * ...
+ *
+ *
+ * this.signInWithApple.signin({})
+ *   .then((res: any) => console.log(res)) // contains token, user's name, email, etc.
+ *   .catch((error: any) => console.error(error));
+ *
+ * ```
+ */
 @Plugin({
   pluginName: 'SignInWithApple',
   repo: 'https://github.com/twogate/cordova-plugin-sign-in-with-apple/blob/master/plugin.xml',
@@ -11,6 +32,11 @@ import { Observable } from 'rxjs';
 })
 @Injectable()
 export class SignInWithApple extends IonicNativePlugin {
+  /**
+   * Show iOS's Sign in with Apple screen
+   * @param opts {any} Sign in with Apple options
+   * @return {Promise<any>} Returns a promise that resolves when complete Sign in with Apple
+   */
   @Cordova()
   signin(opts?: any): Promise<any> {
     return;
