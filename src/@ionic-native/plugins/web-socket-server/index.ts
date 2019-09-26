bd6b8be9583b6de7ea2bc1736c1b90929c994a16
Min <minyin142p@gmail.com>
2019-09-26 21:19:35 +0200

    Change for custom plugin fork

--- src/@ionic-native/plugins/web-socket-server/index.ts
+++ src/@ionic-native/plugins/web-socket-server/index.ts
@@ -9,9 +9,21 @@
  * - Remove this note
  *
  */
-import { Injectable } from '@angular/core';
-import { Plugin, Cordova, CordovaProperty, CordovaInstance, InstanceProperty, IonicNativePlugin } from '@ionic-native/core';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import {
+  Plugin,
+  Cordova,
+  CordovaProperty,
+  CordovaInstance,
+  InstanceProperty,
+  IonicNativePlugin
+} from "@ionic-native/core";
+import { Observable } from "rxjs";
+import { HttpdOptions } from "../httpd";
+import { Http2ServerRequest } from "http2";
+import { HTTPResponse } from "../http";
+import { DownloadHttpHeader } from "../downloader";
+import { UniqueDeviceID } from "../unique-device-id";
 
 /**
  * @name WebSocket Server
@@ -35,40 +47,113 @@ import { Observable } from 'rxjs';
  * ```
  */
 @Plugin({
-  pluginName: 'WebSocketServer',
-  plugin: 'cordova-plugin-websocket-server',
-  pluginRef: 'cordova.plugins.wsserver',
-  repo: 'https://github.com/becvert/cordova-plugin-websocket-server',
-  platforms: ['Android', 'iOS'],
+  pluginName: "WebSocketServer",
+  plugin: "cordova-plugin-websocket-server",
+  pluginRef: "cordova.plugins.wsserver",
+  repo: "https://github.com/becvert/cordova-plugin-websocket-server",
+  platforms: ["Android", "iOS"]
 })
 @Injectable()
 export class WebSocketServer extends IonicNativePlugin {
 
   @Cordova()
-  getInterfaces(): Promise<any> {
+  getInterfaces(): Promise<WebSocketInterfaces> {
     return;
   }
 
   @Cordova({
-      clearFunction: 'stop',
+    observable: true,
+    clearFunction: "stop",
   })
-  start(port: number, options: any): Promise<any> {
+  start(port: number, options: WebSocketOptions): Observable<WebSocketServerDetails> {
     return;
   }
 
   @Cordova()
-  stop(): Promise<any> {
+  onMessage(): Observable<WebSocketMessage> {
+      return;
+  }
+
+  @Cordova()
+  onOpen(): Observable<WebSocketConnection> {
+      return;
+  }
+
+  @Cordova()
+  onClose(): Observable<WebSocketClose> {
+      return;
+  }
+
+  @Cordova()
+  stop(): Promise<WebSocketServerDetails> {
     return;
   }
 
   @Cordova()
-  send(conn: any, msg: any): Promise<any> {
+  send(conn: WebSocketIdentifier, msg: string): Promise<any> {
     return;
   }
 
   @Cordova()
-  close(conn: any, code: any, reason: any): Promise<any> {
+  close(conn: WebSocketIdentifier, code: number, reason: string): Promise<any> {
     return;
   }
+}
 
+export type WebSocketInterfaces = {
+  [key: string]: WebSocketInterface;
+};
+
+export interface WebSocketInterface {
+  ipv4Addresses: string[];
+  ipv6Addresses: string[];
+}
+
+export interface WebSocketOptions {
+  origins?: string[];
+  protocols?: string[];
+  tcpNoDelay?: boolean;
+}
+
+export interface WebSocketIdentifier {
+    uuid: string;
+}
+
+export interface WebSocketServerDetails {
+    addr: string;
+    port: number;
+}
+
+export interface WebSocketMessage {
+    conn: WebSocketConnection;
+    msg: string;
+}
+
+export interface WebSocketClose {
+    conn: WebSocketConnection;
+    code: number;
+    reason: string;
+    wasClean: boolean;
+}
+
+export interface WebSocketConnection extends WebSocketIdentifier {
+    remoteAttr: string;
+    state: 'open' | 'closed';
+    httpFields: HttpFields;
+    resource: string;
+}
+
+export interface HttpFields {
+    'Accept-Encoding': string;
+    'Accept-Language': string;
+    'Cache-Control': string;
+    Connection: string;
+    Host: string;
+    Origin: string;
+    Pragma: string;
+    'Sec-WebSocket-Extensions': string;
+    'Sec-WebSocket-Key': string;
+    'Sec-WebSocket-Version': string;
+    Upgrade: string;
+    'User-Agent': string;
 }
