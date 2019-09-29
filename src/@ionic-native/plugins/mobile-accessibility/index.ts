609a168f964dec460cd69df8fd75529d28ac1f94
Daniel Sogl <daniel@sogls.de>
2019-09-29 13:23:16 +0200

    Update index.ts

--- src/@ionic-native/plugins/mobile-accessibility/index.ts
+++ src/@ionic-native/plugins/mobile-accessibility/index.ts
@@ -2,7 +2,7 @@ import { Cordova, IonicNativePlugin, Plugin } from '@ionic-native/core';
 import { Injectable } from '@angular/core';
 
 /**
- * @name MobileAccessibility
+ * @name Mobile Accessibility
  * @description
  * This plugin exposes information on the status of various accessibility features of mobile operating systems, including, for example, whether a screen reader is running, invert colors is enabled, and the preferred scaling for text.
  * It also allows an application to send a string to be spoken by the screen reader, or a command to stop the screen reader from speaking.
