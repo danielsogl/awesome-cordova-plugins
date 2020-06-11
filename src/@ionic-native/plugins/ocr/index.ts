dc6e3313bbe55dda543b47bb316eeb8fa49308cd
Jakob Engelbrecht <jakob@basher.dk>
2020-06-11 17:59:34 +0200

    docs(our): add deprecation warning (#3439)

--- src/@ionic-native/plugins/ocr/index.ts
+++ src/@ionic-native/plugins/ocr/index.ts
@@ -119,7 +119,9 @@ export interface OCRResult {
  * @name OCR
  * @description
  * This plugin attempts to identify and extract text from an image.
- *
+ * Please note: This plugin depends on the GoogleMobileVision pod which is referencing UIWebview, that has been deprecated by Apple.
+ * Don't use this plugin in an app intended for App Store as you will get a review rejection from Apple: `Deprecated API Usage — Apple will stop accepting submissions of apps that use UIWebView APIs`
+ * For more info, please see the following Github issue [Google Mobile Vision relying on deprecated UIWebview](https://github.com/NeutrinosPlatform/cordova-plugin-mobile-ocr/issues/27).
  * @usage
  * ```typescript
  * import { OCR, OCRSourceType } from '@ionic-native/ocr/ngx';
