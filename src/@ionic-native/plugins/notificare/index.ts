cd2ca51629c943035fb41ab8bf8265d40addbefb
Helder Pinhal <helder@notifica.re>
2020-06-30 10:30:54 +0200

    feat(notificare): add all method matching the Cordova plugin

--- src/@ionic-native/plugins/notificare/index.ts
+++ src/@ionic-native/plugins/notificare/index.ts
@@ -10,15 +10,7 @@
  *
  */
 import { Injectable } from '@angular/core';
-import {
-  Plugin,
-  Cordova,
-  CordovaProperty,
-  CordovaInstance,
-  InstanceProperty,
-  IonicNativePlugin,
-} from '@ionic-native/core';
-import { Observable } from 'rxjs';
+import { Cordova, IonicNativePlugin, Plugin } from '@ionic-native/core';
 
 /**
  * @name Notificare
@@ -66,95 +58,150 @@ export class Notificare extends IonicNativePlugin {
     return;
   }
 
-  /**
-   *
-   * @param name
-   * @param data
-   * @returns {Promise<void>}
-   */
   @Cordova()
-  logCustomEvent(name: string, data: Record<string, any>): Promise<void> {
-    return;
-  }
-
-  /**
-   *
-   * @param event
-   * @param callback
-   * @returns {Promise<any>}
-   */
-  @Cordova()
-  on(event: string, callback: (data: any) => void): void {
+  registerForNotifications(): Promise<void> {
     return;
   }
 
   @Cordova()
-  login(email: string, password: string): Promise<void> {
+  unregisterForNotifications(): Promise<void> {
+    return;
+  }
+
+  // new
+  @Cordova()
+  setAuthorizationOptions(options: string[]): Promise<void> {
+    return;
+  }
+
+  // new
+  @Cordova()
+  setPresentationOptions(options: string[]): Promise<void> {
+    return;
+  }
+
+  // new
+  @Cordova()
+  setCategoryOptions(options: string[]): Promise<void> {
     return;
   }
 
   @Cordova()
-  fetchAccountDetails(): Promise<any> {
+  isRemoteNotificationsEnabled(): Promise<boolean> {
     return;
   }
 
   @Cordova()
-  createAccount(email: string, name: string, password: string): Promise<void> {
+  isAllowedUIEnabled(): Promise<boolean> {
+    return;
+  }
+
+  // new
+  @Cordova()
+  isNotificationFromNotificare(notification: any): Promise<boolean> {
+    return;
+  }
+
+  // new
+  @Cordova()
+  fetchNotificationSettings(): Promise<any> {
     return;
   }
 
   @Cordova()
-  sendPassword(email: string): Promise<void> {
+  startLocationUpdates(): Promise<void> {
     return;
   }
 
   @Cordova()
-  resetPassword(password: string, token: string): Promise<void> {
+  stopLocationUpdates(): Promise<void> {
+    return;
+  }
+
+  // new
+  @Cordova()
+  clearLocation(): Promise<void> {
     return;
   }
 
   @Cordova()
-  fetchUserPreferences(): Promise<any[]> {
+  isLocationServicesEnabled(): Promise<boolean> {
     return;
   }
 
   @Cordova()
-  logout(): Promise<void> {
+  enableBeacons(): Promise<void> {
     return;
   }
 
   @Cordova()
-  generateAccessToken(): Promise<void> {
+  disableBeacons(): Promise<void> {
+    return;
+  }
+
+  // new
+  @Cordova()
+  registerDevice(userId: Nullable<string>, userName: Nullable<string>): Promise<void> {
+    return;
+  }
+
+  // new
+  @Cordova()
+  fetchDevice(): Promise<any> {
+    return;
+  }
+
+  // new
+  @Cordova()
+  fetchPreferredLanguage(): Promise<string> {
+    return;
+  }
+
+  // new
+  @Cordova()
+  updatePreferredLanguage(preferredLanguage: string): Promise<void> {
     return;
   }
 
   @Cordova()
-  addSegmentToUserPreference(segment: any, preference: any): Promise<void> {
+  fetchTags(): Promise<string[]> {
     return;
   }
 
   @Cordova()
-  removeSegmentFromUserPreference(segment: any, preference: any): Promise<void> {
+  addTag(tag: string): Promise<void> {
     return;
   }
 
   @Cordova()
-  changePassword(password: string): Promise<void> {
+  addTags(tags: string[]): Promise<void> {
     return;
   }
 
   @Cordova()
-  validateAccount(token: string): Promise<void> {
+  removeTag(tag: string): Promise<void> {
     return;
   }
 
   @Cordova()
-  fetchAssets(group: string): Promise<any[]> {
+  removeTags(tags: string): Promise<void> {
     return;
   }
 
   @Cordova()
-  isLoggedIn(): Promise<boolean> {
+  clearTags(): Promise<void> {
+    return;
+  }
+
+  // new
+  @Cordova()
+  fetchUserData(): Promise<any> {
+    return;
+  }
+
+  // new
+  @Cordova()
+  updateUserData(userData: any): Promise<void> {
     return;
   }
 
@@ -173,63 +220,21 @@ export class Notificare extends IonicNativePlugin {
     return;
   }
 
+  // new
   @Cordova()
-  addTag(tag: string): Promise<void> {
+  fetchNotification(notification: any): Promise<any> {
     return;
   }
 
+  // new
   @Cordova()
-  removeTag(tag: string): Promise<void> {
+  fetchNotificationForInboxItem(inboxItem: any): Promise<any> {
     return;
   }
 
+  // new
   @Cordova()
-  fetchTags(): Promise<string[]> {
-    return;
-  }
-
-  @Cordova()
-  isRemoteNotificationsEnabled(): Promise<boolean> {
-    return;
-  }
-
-  @Cordova()
-  isAllowedUIEnabled(): Promise<boolean> {
-    return;
-  }
-
-  @Cordova()
-  registerForNotifications(): Promise<void> {
-    return;
-  }
-
-  @Cordova()
-  unregisterForNotifications(): Promise<void> {
-    return;
-  }
-
-  @Cordova()
-  isLocationServicesEnabled(): Promise<boolean> {
-    return;
-  }
-
-  @Cordova()
-  startLocationUpdates(): Promise<void> {
-    return;
-  }
-
-  @Cordova()
-  stopLocationUpdates(): Promise<void> {
-    return;
-  }
-
-  @Cordova()
-  enableBeacons(): Promise<void> {
-    return;
-  }
-
-  @Cordova()
-  disableBeacons(): Promise<void> {
+  presentNotification(notification: any): Promise<void> {
     return;
   }
 
@@ -244,17 +249,170 @@ export class Notificare extends IonicNativePlugin {
   }
 
   @Cordova()
-  markAsRead(item: any): Promise<void> {
-    return;
-  }
-
-  @Cordova()
   removeFromInbox(item: any): Promise<void> {
     return;
   }
 
   @Cordova()
+  markAsRead(item: any): Promise<void> {
+    return;
+  }
+
+  @Cordova()
   clearInbox(): Promise<void> {
+    return;
+  }
+
+  @Cordova()
+  fetchAssets(group: string): Promise<any[]> {
+    return;
+  }
+
+  // new
+  @Cordova()
+  fetchPassWithSerial(serial: string): Promise<any> {
+    return;
+  }
+
+  // new
+  @Cordova()
+  fetchPassWithBarcode(barcode: string): Promise<any> {
+    return;
+  }
+
+  // new
+  @Cordova()
+  fetchProducts(): Promise<any[]> {
+    return;
+  }
+
+  // new
+  @Cordova()
+  fetchPurchasedProducts(): Promise<any[]> {
+    return;
+  }
+
+  // new
+  @Cordova()
+  fetchProduct(product: any): Promise<any> {
+    return;
+  }
+
+  // new
+  @Cordova()
+  buyProduct(product: any): Promise<void> {
+    return;
+  }
+
+  @Cordova()
+  enableBilling(): Promise<void> {
+    return;
+  }
+
+  @Cordova()
+  disableBilling(): Promise<void> {
+    return;
+  }
+
+  /**
+   *
+   * @param name
+   * @param data
+   * @returns {Promise<void>}
+   */
+  @Cordova()
+  logCustomEvent(name: string, data: Record<string, any>): Promise<void> {
+    return;
+  }
+
+  @Cordova()
+  logOpenNotification(notification: any): Promise<void> {
+    return;
+  }
+
+  @Cordova()
+  logInfluencedNotification(notification: any): Promise<void> {
+    return;
+  }
+
+  @Cordova()
+  logReceiveNotification(notification: any): Promise<void> {
+    return;
+  }
+
+  @Cordova()
+  doCloudHostOperation(
+    verb: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE',
+    path: string,
+    headers: Nullable<Record<string, string>>,
+    params: Nullable<Record<string, string>>,
+    body: Nullable<any>
+  ): Promise<any> {
+    return;
+  }
+
+  @Cordova()
+  createAccount(email: string, name: string, password: string): Promise<void> {
+    return;
+  }
+
+  @Cordova()
+  validateAccount(token: string): Promise<void> {
+    return;
+  }
+
+  @Cordova()
+  resetPassword(password: string, token: string): Promise<void> {
+    return;
+  }
+
+  @Cordova()
+  sendPassword(email: string): Promise<void> {
+    return;
+  }
+
+  @Cordova()
+  login(email: string, password: string): Promise<void> {
+    return;
+  }
+
+  @Cordova()
+  logout(): Promise<void> {
+    return;
+  }
+
+  @Cordova()
+  isLoggedIn(): Promise<boolean> {
+    return;
+  }
+
+  @Cordova()
+  generateAccessToken(): Promise<void> {
+    return;
+  }
+
+  @Cordova()
+  changePassword(password: string): Promise<void> {
+    return;
+  }
+
+  @Cordova()
+  fetchAccountDetails(): Promise<any> {
+    return;
+  }
+
+  @Cordova()
+  fetchUserPreferences(): Promise<any[]> {
+    return;
+  }
+
+  @Cordova()
+  addSegmentToUserPreference(segment: any, preference: any): Promise<void> {
+    return;
+  }
+
+  @Cordova()
+  removeSegmentFromUserPreference(segment: any, preference: any): Promise<void> {
     return;
   }
 
@@ -268,14 +426,14 @@ export class Notificare extends IonicNativePlugin {
     return;
   }
 
+  /**
+   *
+   * @param event
+   * @param callback
+   * @returns {Promise<any>}
+   */
   @Cordova()
-  doCloudHostOperation(
-    verb: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE',
-    path: string,
-    headers: Nullable<Record<string, string>>,
-    params: Nullable<Record<string, string>>,
-    body: Nullable<any>
-  ): Promise<any> {
+  on(event: string, callback: (data: any) => void): void {
     return;
   }
 }
