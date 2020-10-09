1694ce47302a6cdde1bae89938957276c5d8c72b
jcesarmobile <jcesarmobile@gmail.com>
2020-10-09 17:13:21 +0200

    Change InAppPurchase plugin id
    it was renamed from cc.fovea.cordova.purchase to cordova-plugin-purchase

--- src/@ionic-native/plugins/in-app-purchase-2/index.ts
+++ src/@ionic-native/plugins/in-app-purchase-2/index.ts
@@ -541,11 +541,11 @@ export class IAPError {
  */
 @Plugin({
   pluginName: 'InAppPurchase2',
-  plugin: 'cc.fovea.cordova.purchase',
+  plugin: 'cordova-plugin-purchase',
   pluginRef: 'store',
   repo: 'https://github.com/j3k0/cordova-plugin-purchase',
   platforms: ['iOS', 'Android', 'Windows'],
-  install: 'ionic cordova plugin add cc.fovea.cordova.purchase --variable BILLING_KEY="<ANDROID_BILLING_KEY>"',
+  install: 'ionic cordova plugin add cordova-plugin-purchase --variable BILLING_KEY="<ANDROID_BILLING_KEY>"',
 })
 @Injectable()
 export class InAppPurchase2 extends IonicNativePlugin {
