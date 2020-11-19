0f197ad9fcd4e56c0c61bd87c6aec76889af6ab2
aboedo <andresboedo@gmail.com>
2020-11-19 04:35:49 -0300

    feat(purchases): update to plugin version 1.3.2 (#3566)

--- src/@ionic-native/plugins/purchases/index.ts
+++ src/@ionic-native/plugins/purchases/index.ts
@@ -207,7 +207,7 @@ export enum INTRO_ELIGIBILITY_STATUS {
  */
 @Plugin({
   pluginName: 'Purchases',
-  plugin: 'cordova-plugin-purchases@1.3.0',
+  plugin: 'cordova-plugin-purchases@1.3.2',
   pluginRef: 'Purchases', // the variable reference to call the plugin, example: navigator.geolocation
   repo: 'https://github.com/RevenueCat/cordova-plugin-purchases', // the github repository URL for the plugin
   platforms: ['Android', 'iOS'], // Array of platforms supported, example: ['Android', 'iOS']
