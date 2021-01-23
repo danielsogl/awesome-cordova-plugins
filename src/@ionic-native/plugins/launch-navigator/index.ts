6b18c3d0614db1f525aaecfb9e09067537061ba9
capc0 <s.hagemann@hs-solutions.de>
2021-01-23 18:16:34 +0100

    fix(launch-navigator): enableGeolocation and enableGeocoding typings (#3584)
    based of https://github.com/dpa99c/phonegap-launch-navigator/blob/master/uk.co.workingedge.phonegap.plugin.launchnavigator.d.ts
    
    Co-authored-by: Daniel Sogl <mytechde@outlook.com>

--- src/@ionic-native/plugins/launch-navigator/index.ts
+++ src/@ionic-native/plugins/launch-navigator/index.ts
@@ -168,12 +168,12 @@ export interface LaunchNavigatorOptions {
   launchModeAppleMaps?: string;
 
   /**
-   * If true, and input location type(s) doesn't match those required by the app, use geocoding to obtain the address/coords as required. Defaults to true.
+   * (Windows only) If false, the plugin will NOT attempt to use the geolocation plugin to determine the current device position when the start location parameter is omitted. Defaults to true.
    */
   enableGeolocation?: boolean;
 
   /**
-   * If true, and input location type(s) doesn't match those required by the app, use geocoding to obtain the address/coords as required. Defaults to true.
+   * (Android and iOS only) If true, and input location type(s) doesn't match those required by the app, use geocoding to obtain the address/coords as required. Defaults to true.
    */
   enableGeocoding?: boolean;
 
