ed7b506e6db81fc688eeede16b6fd03048018808
zafirskthelifehacker <zafirskthelifehacker@hotmail.com>
2022-02-08 11:57:00 +0400

    Remove ccordova

--- src/@awesome-cordova-plugins/plugins/background-upload/index.ts
+++ src/@awesome-cordova-plugins/plugins/background-upload/index.ts
@@ -95,7 +95,7 @@ export class FileTransferManager {
  */
 @Plugin({
   pluginName: 'BackgroundUpload',
-  plugin: '@spoonconsulting/ccordova-plugin-background-upload',
+  plugin: '@spoonconsulting/cordova-plugin-background-upload',
   pluginRef: 'FileTransferManager',
   repo: 'https://github.com/spoonconsulting/cordova-plugin-background-upload',
   install: 'ionic cordova plugin add @spoonconsulting/cordova-plugin-background-upload',
