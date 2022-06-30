19fc222ea93c79c53156aa0a05b3dc0bffa77a7e
Alex Ryltsov <ryltsov.alex@gmail.com>
2022-06-30 11:58:43 +0300

    fix(diagnostic): adjust options declaration (#4227)

--- src/@awesome-cordova-plugins/plugins/diagnostic/index.ts
+++ src/@awesome-cordova-plugins/plugins/diagnostic/index.ts
@@ -91,9 +91,9 @@ export class Diagnostic extends AwesomeCordovaNativePlugin {
    * iOS ONLY
    * Location accuracy authorization
    */
-  locationAccuracyAuthorization: {
-    FULL: 'full';
-    REDUCED: 'reduced';
+  locationAccuracyAuthorization = {
+    FULL: 'full',
+    REDUCED: 'reduced',
   };
 
   permissionGroups = {
