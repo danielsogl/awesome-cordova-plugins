1cb2ce808109c888121a5a451d043a1617433a60
Cesar de la Vega <cesarvegaro@gmail.com>
2022-11-03 14:20:58 +0100

    update version to 3.2.1

--- src/@awesome-cordova-plugins/plugins/purchases/index.ts
+++ src/@awesome-cordova-plugins/plugins/purchases/index.ts
@@ -213,7 +213,7 @@ export enum INTRO_ELIGIBILITY_STATUS {
  */
 @Plugin({
   pluginName: 'Purchases',
-  plugin: 'cordova-plugin-purchases@3.2.0',
+  plugin: 'cordova-plugin-purchases@3.2.1',
   pluginRef: 'Purchases', // the variable reference to call the plugin, example: navigator.geolocation
   repo: 'https://github.com/RevenueCat/cordova-plugin-purchases', // the github repository URL for the plugin
   platforms: ['Android', 'iOS'], // Array of platforms supported, example: ['Android', 'iOS']
