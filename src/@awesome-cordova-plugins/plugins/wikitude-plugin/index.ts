5aeda7e5c41fdce401c8d84c1b3d8fb297a16aed
Gelinger Media <team@gelinger.at>
2023-04-19 21:28:57 +0200

    removed unneccesary comments

--- src/@awesome-cordova-plugins/plugins/wikitude-plugin/index.ts
+++ src/@awesome-cordova-plugins/plugins/wikitude-plugin/index.ts
@@ -1,14 +1,3 @@
-/**
- * This is a template for new plugin wrappers
- *
- * TODO:
- * - Add/Change information below
- * - Document usage (importing, executing main functionality)
- * - Remove any imports that you are not using
- * - Remove all the comments included in this template, EXCEPT the @Plugin wrapper docs and any other docs you added
- * - Remove this note
- *
- */
 import { Injectable } from '@angular/core';
 import {
   Plugin,
@@ -23,7 +12,7 @@ import { Observable } from 'rxjs';
 /**
  * @name Wikitude Plugin
  * @description
- * This plugin does something
+ * This plugin  helps to bridge the functions in Cordova Plugin of Wikitude to Ionic/Angular for Capacitor
  *
  * @usage
  * ```typescript
@@ -43,32 +32,20 @@ import { Observable } from 'rxjs';
  */
 @Plugin({
   pluginName: 'WikitudePlugin',
-  plugin: 'com.wikitude.phonegap.wikitudeplugin', // npm package name, example: cordova-plugin-camera
-  pluginRef: 'WikitudePlugin', // the variable reference to call the plugin, example: navigator.geolocation
-  repo: 'git+https://github.com/Wikitude/wikitude-phonegap.git', // the github repository URL for the plugin
-  install: '', // OPTIONAL install command, in case the plugin requires variables
-  installVariables: [], // OPTIONAL the plugin requires variables
-  platforms: ['Android', 'iOS'], // Array of platforms supported, example: ['Android', 'iOS']
+  plugin: 'com.wikitude.phonegap.wikitudeplugin',
+  pluginRef: 'WikitudePlugin',
+  repo: 'git+https://github.com/Wikitude/wikitude-phonegap.git',
+  install: '',
+  installVariables: [],
+  platforms: ['Android', 'iOS'],
 })
 @Injectable()
 export class WikitudePlugin extends AwesomeCordovaNativePlugin {
-  /**
-   * This function does something
-   * @param arg1 {string} Some param to configure something
-   * @param arg2 {number} Another param to configure something
-   * @return {Promise<any>} Returns a promise that resolves when something happens
-   */
   @Cordova()
   isDeviceSupported(successCallback: any, errorCallback: any, requiredFeatures: any): Promise<void> {
     return; // We add return; here to avoid any IDE / Compiler errors
   }
 
-  /**
-   * This function does something
-   * @param arg1 {string} Some param to configure something
-   * @param arg2 {number} Another param to configure something
-   * @return {Promise<any>} Returns a promise that resolves when something happens
-   */
   @Cordova()
   loadARchitectWorld(
     successCallback: any,
@@ -77,22 +54,22 @@ export class WikitudePlugin extends AwesomeCordovaNativePlugin {
     requiredFeatures: any,
     startupConfiguration: any
   ): Promise<void> {
-    return; // We add return; here to avoid any IDE / Compiler errors
+    return;
   }
 
   @Cordova()
   close(): Promise<void> {
-    return; // We add return; here to avoid any IDE / Compiler errors
+    return;
   }
 
   @Cordova()
   requestAccess(successCallback: any, errorCallback: any, requiredFeatures: any): Promise<void> {
-    return; // We add return; here to avoid any IDE / Compiler errors
+    return;
   }
 
   @Cordova()
   setOnUrlInvokeCallback(onUrlInvokeCallback: any): Promise<void> {
-    return; // We add return; here to avoid any IDE / Compiler errors
+    return;
   }
 
   @Cordova()
