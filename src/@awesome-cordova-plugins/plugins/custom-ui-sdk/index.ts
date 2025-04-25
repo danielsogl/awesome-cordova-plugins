34f248b56d3d9b732a49f81f9e88f2363f0a05bb
Raj Goenka <raj.goenka@paytm.com>
2025-04-25 14:49:29 +0530

    feat(customuisdk) : add paytmpayments plugin support

--- src/@awesome-cordova-plugins/plugins/custom-ui-sdk/index.ts
+++ src/@awesome-cordova-plugins/plugins/custom-ui-sdk/index.ts
@@ -1,13 +1,6 @@
 /**
  * This is a template for new plugin wrappers
  *
- * TODO:
- * - Add/Change information below
- * - Document usage (importing, executing main functionality)
- * - Remove any imports that you are not using
- * - Remove all the comments included in this template, EXCEPT the @Plugin wrapper docs and any other docs you added
- * - Remove this note
- *
  */
 import { Injectable } from '@angular/core';
 import {
